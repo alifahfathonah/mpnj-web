47f3492b32c0e8e5e7d44e699d9bae3212b563c6
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-30 22:38:44 +0700

    define produk model berdasarkan database

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    //
+    protected $table = 'produk';
+    protected $primaryKey = 'id_produk';
+    protected $fillable = ['nama_produk','satuan','berat','harga_modal','harga_jual','diskon','stok','keterangan','tipe_produk','foto','wishlist','terjual'];
 }
