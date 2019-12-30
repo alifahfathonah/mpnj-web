7ea1eef5d39fae5b06481084c199ef35562125d1
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-30 22:39:41 +0700

    membuat fungsi index untuk menampilkan data produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class ProdukController extends Controller
 {
-    //
+    public function index()
+    {
+        $data['produk'] = Produk::where('pelapak_id', 1)->get();
+        return view('pelapak/produk/data_produk', $data);
+    }
 }
