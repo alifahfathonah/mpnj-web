a3bffc14be596e556c7429f8d6e763ef4427ae6f
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-31 15:12:10 +0700

    scripts untuk upload multiple foto

--- resources/views/pelapak/produk/tambah_produk.blade.php
+++ resources/views/pelapak/produk/tambah_produk.blade.php
@@ -85,3 +85,63 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+<script>
+    Dropzone.autoDiscover = false;
+    $(function() {
+        // Dropzone.autoDiscover = true;
+        var uploadedDocumentMap = {}
+        var upload = new Dropzone("#document-dropzone", {
+            url: '/administrator/produk/upload_foto',
+            method:"post",
+            paramName: "file",
+            maxFilesize: 2, // MB
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            success: function (file, response) {
+                $('form').append('<input type="hidden" name="document[]" value="' + response.name + '">')
+                uploadedDocumentMap[file.name] = response.name
+            },
+            removedfile: function (file) {
+                file.previewElement.remove()
+                var name = ''
+                if (typeof file.file_name !== 'undefined') {
+                    name = file.file_name
+                } else {
+                    name = uploadedDocumentMap[file.name]
+                    $.ajax({
+                        url: '/administrator/produk/unlink',
+                        type: 'POST',
+                        data: {
+                            'name': name
+                        },
+                        headers: {
+                            'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                        },
+                        dataType: 'json',
+                        success: function(response) {
+                            console.log(response);
+                        }
+                    });
+                }
+                $('form').find('input[name="document[]"][value="' + name + '"]').remove()
+            },
+            init: function() {
+                @if(isset($project) && $project->document)
+                    var files =
+                    {!! json_encode($project->document) !!}
+                    for (var i in files) {
+                        var file = files[i]
+                        this.options.addedfile.call(this, file)
+                        file.previewElement.classList.add('dz-complete')
+                        $('form').append('<input type="hidden" name="document[]" value="' + file.file_name + '">')
+                    }
+                @endif
+            }
+        });
+    })
+</script>
+@endpush
