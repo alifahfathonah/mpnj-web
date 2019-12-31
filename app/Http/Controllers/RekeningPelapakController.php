8c1c7b28057630c7e43fc151d498d966a55c3abb
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-31 11:24:55 +0700

    membuat fungsi menampilkan form tambah dan buat fungsi simpan

--- app/Http/Controllers/RekeningPelapakController.php
+++ app/Http/Controllers/RekeningPelapakController.php
@@ -9,7 +9,26 @@ class RekeningPelapakController extends Controller
 {
     public function index()
     {
-        $data['rekening'] = Rekening_Pelapak::all();
+        $data['rekening'] = Rekening_Pelapak::where('pelapak_id', 1)->get();
         return view('pelapak/rekening/data_rekening', $data);
+    }
+
+    public function tambah()
+    {
+        return view('pelapak/rekening/tambah_rekening');
+    }
+
+    public function simpan(Request $request)
+    {
+        $simpanRekening = Rekening_Pelapak::create([
+            'nama_bank' => $request->nama_bank,
+            'nomor_rekening' => $request->nomor_rekening,
+            'atas_nama' => $request->atas_nama,
+            'pelapak_id' => 1
+        ]);
+
+        if ($simpanRekening) {
+            return redirect('administrator/rekening');
+        }
     }
 }
