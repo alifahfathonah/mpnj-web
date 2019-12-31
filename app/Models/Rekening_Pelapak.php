8ca99e9fec0b48e13aa43ec6d7e848e3e01ce9c0
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-31 11:06:44 +0700

    Membuat model rekening pelapak

---
+++ app/Models/Rekening_Pelapak.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rekening_Pelapak extends Model
+{
+    protected $table = 'rekening_pelapak';
+    protected $primaryKey = 'id_rekening';
+    protected $fillable = ['nama_bank','nomor_rekening','atas_nama','pelapak_id'];
+}
