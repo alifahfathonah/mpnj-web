0fc39897ce497515af31e2be77fe31a8f60b4dc4
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-04 11:48:57 +0700

    membuat halaman web master

---
+++ resources/views/web/web_master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MP</title>
+    <link rel="stylesheet" href="{{ asset('assets/web/css/bootstrap.min.css') }}">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="/produk">produk</a>
+            </div>
+        </div>
+    </nav>
+
+    <br>
+
+    <div class="container">
+        @yield('web_konten')
+    </div>
+
+    <script src="{{ asset('assets/admin/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/web/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
