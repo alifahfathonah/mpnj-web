d203adb68c590a30cb188ebd8245c9b03abba3f3
LuthfiRobit <luthfirobit@gmail.com>
2020-01-08 23:26:03 +0700

    Membuat API Create Register

---
+++ app/Http/Controllers/Api/RegisterKonsumenController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Konsumen;
+use Illuminate\Http\Request;
+
+class RegisterKonsumenController extends Controller
+{
+    public function create(request $request)
+    {
+        $konsumen = new Konsumen;
+        $konsumen->nama_lengkap = $request->nama_lengkap;
+        $konsumen->username = $request->username;
+        $konsumen->password = $request->password;
+        $konsumen->provinsi_id = $request->provinsi_id;
+        $konsumen->city_id = $request->city_id;
+        $konsumen->alamat = $request->alamat;
+        $konsumen->kode_pos = $request->kode_pos;
+        $konsumen->nomor_hp = $request->nomor_hp;
+        $konsumen->email = $request->email;
+        $konsumen->status = $request->status;
+
+        $konsumen->save();
+    }
+}
