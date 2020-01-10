007feb4e08451512945a25a40d3beab40d453109
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-10 09:33:13 +0700

    perubahan tampilan halaman keranjang

--- resources/views/web/web_keranjang.blade.php
+++ resources/views/web/web_keranjang.blade.php
@@ -31,112 +31,116 @@
     =================================-->
 
 <!--================================
-            START DASHBOARD AREA
+            START SIGNUP AREA
     =================================-->
-<section class="cart_area section--padding2 bgcolor">
+<section class="section--padding2 bgcolor">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="product_archive added_to__cart">
-                    <div class="title_area">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <h4>Daftar Barang dalam Keranjang</h4>
+                <div class="">
+                    <div class="modules__content">
+                        <div class="withdraw_module withdraw_history">
+                            <div class="withdraw_table_header">
+                                <h3>Daftar Barang dalam Keranjang</h3>
                             </div>
-                            <div class="col-md-3">
-                                <h4 class="add_info">Kategori</h4>
-                            </div>
-                            <div class="col-md-2">
-                                <h4>Harga</h4>
-                            </div>
-                            <div class="col-md-2">
-                                <h4>Hapus</h4>
+                            <div class="table-responsive">
+                                <table class="table withdraw__table">
+                                    <thead>
+                                        <tr>
+                                            <th><input type="checkbox" name="pilih" id="pilih"></th>
+                                            <th>Barang</th>
+                                            <th>Kategori</th>
+                                            <th>Harga</th>
+                                            <th>Qty</th>
+                                            <th>Sub Harga</th>
+                                            <th>Hapus</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach ($keranjang as $k)
+                                        <tr>
+                                            <td>
+                                                <input type="checkbox" name="check" id="check">
+                                            </td>
+                                            <td>
+                                                <div class="product__description">
+                                                    <img src="{{ asset('assets/foto_produk/'.$k->produk->foto_produk[0]->foto_produk) }}"
+                                                        alt="Purchase image" width="100">
+                                                    <div class="short_desc">
+                                                        <a href="single-product.html">
+                                                            <h4>{{ $k->produk->nama_produk }}</h4>
+                                                        </a>
+                                                        {{-- <p>Nunc placerat mi id nisi inter dum mollis. Praesent phare...</p> --}}
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>{{ $k->produk->kategori->nama_kategori }}</td>
+                                            <td class="bold" id="harga{{ $loop->iteration }}">
+                                                @currency($k->produk->harga_jual)
+                                            </td>
+                                            <td style="width: 10%"><input type="number" name="qty" id="qty{{ $loop->iteration }}"
+                                                    class="form-control form-control-sm" value="1"
+                                                    onchange="ubahSubTotal({{ $loop->iteration }})"></td>
+                                            <td id="subHarga{{ $loop->iteration }}">@currency($k->produk->harga_jual)</td>
+                                            <td class="pending">
+                                                <a href="/keranjang/hapus/{{ $k->id_keranjang }}">
+                                                    <span>Hapus</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <div class="row">
+                                    <div class="col-md-6 offset-md-6">
+                                        <div class="cart_calculation">
+                                            {{-- <div class="cart--subtotal">
+                                                <p>
+                                                    <span>Cart Subtotal</span>$120</p>
+                                            </div> --}}
+                                            <div class="cart--total">
+                                                <p>
+                                                    <span>total</span><span id="total">@currency($total)</span></p>
+                                            </div>
+
+                                            <a href="checkout.html" class="btn btn--round btn--md checkout_link">Lanjut
+                                                Checkout</a>
+                                        </div>
+                                    </div>
+                                    <!-- end .col-md-12 -->
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
-                        @foreach ($keranjang as $k)
-                        <div class="col-md-12">
-                            <div class="single_product clearfix">
-                                <div class="col-lg-5 col-md-7 v_middle">
-                                    <div class="product__description">
-                                        <img src="{{ asset('assets/foto_produk/'.$k->produk->foto_produk[0]->foto_produk) }}" alt="Purchase image" width="100">
-                                        <div class="short_desc">
-                                            <a href="single-product.html">
-                                                <h4>{{ $k->produk->nama_produk }}</h4>
-                                            </a>
-                                            {{-- <p>Nunc placerat mi id nisi inter dum mollis. Praesent phare...</p> --}}
-                                        </div>
-                                    </div>
-                                    <!-- end /.product__description -->
-                                </div>
-                                <!-- end /.col-md-5 -->
-
-                                <div class="col-lg-3 col-md-2 v_middle">
-                                    <div class="product__additional_info">
-                                        <ul>
-                                            <li>
-                                                <a href="#">
-                                                    <img src="images/catword.png" alt="">{{ $k->produk->kategori->nama_kategori }}</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <!-- end /.product__additional_info -->
-                                </div>
-                                <!-- end /.col-md-3 -->
-
-                                <div class="col-lg-4 col-md-3 v_middle">
-                                    <div class="product__price_download">
-                                        <div class="item_price v_middle">
-                                            <span>@currency($k->produk->harga_jual)</span>
-                                        </div>
-                                        <div class="item_action v_middle">
-                                            <a href="/keranjang/hapus/{{ $k->id_keranjang }}" class="remove_from_cart">
-                                                <span class="lnr lnr-trash"></span>
-                                            </a>
-                                        </div>
-                                        <!-- end /.item_action -->
-                                    </div>
-                                    <!-- end /.product__price_download -->
-                                </div>
-                                <!-- end /.col-md-4 -->
-                            </div>
-                            <!-- end /.single_product -->
-                        </div>
-                        @endforeach
-                    </div>
-                    <!-- end /.row -->
-
-                    <div class="row">
-                        <div class="col-md-6 offset-md-6">
-                            <div class="cart_calculation">
-                                {{-- <div class="cart--subtotal">
-                                    <p>
-                                        <span>Cart Subtotal</span>$120</p>
-                                </div> --}}
-                                <div class="cart--total">
-                                    <p>
-                                        <span>total</span>@currency($k->produk->sum('harga_jual'))</p>
-                                </div>
-
-                                <a href="checkout.html" class="btn btn--round btn--md checkout_link">Lanjut Checkout</a>
-                            </div>
-                        </div>
-                        <!-- end .col-md-12 -->
-                    </div>
-                    <!-- end .row -->
                 </div>
-                <!-- end /.product_archive2 -->
             </div>
-            <!-- end .col-md-12 -->
+            <!-- end .col-md-6 -->
         </div>
         <!-- end .row -->
-
     </div>
     <!-- end .container -->
 </section>
 <!--================================
-            END DASHBOARD AREA
+            END SIGNUP AREA
     =================================-->
+
+
 @endsection
+
+@push('scripts')
+<script>
+    function ubahSubTotal(n) {
+            let qty = $("#qty"+n).val();
+            var m = $("#harga"+n).html();
+            var split = m.split("Rp. ");
+            var p = split[1].replace('.','');
+
+            $("#subHarga"+n).html("Rp. " + numberFormat(qty * p));
+        }
+
+        function numberFormat(num) {
+            return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+        }
+</script>
+@endpush
