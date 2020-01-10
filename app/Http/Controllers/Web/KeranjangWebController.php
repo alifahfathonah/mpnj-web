cfbd5b95f3ddc04d94d98bbdc2060c1c1fb809bc
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-11 01:07:48 +0700

    group by toko untuk keranjang

--- app/Http/Controllers/Web/KeranjangWebController.php
+++ app/Http/Controllers/Web/KeranjangWebController.php
@@ -5,22 +5,20 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Keranjang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KeranjangWebController extends Controller
 {
     public function index(Request $request)
     {
         $konsumen_id = $request->session()->get('id_konsumen', 0);
-        $data['keranjang'] = Keranjang::with(['produk', 'konsumen'])->where('konsumen_id', $konsumen_id)->get();
+        $data['keranjang'] = Keranjang::with(['produk', 'konsumen'])->where('konsumen_id', 1)->get()->groupBy('produk.pelapak.nama_toko');
+        $data['total'] = DB::table('keranjang')->join('produk', 'keranjang.produk_id', '=', 'produk.id_produk')->where('keranjang.konsumen_id', $konsumen_id)->sum('produk.harga_jual');
         return view('web/web_keranjang', $data);
     }
 
     public function simpan(Request $request)
     {
-        // $cek = Keranjang::where('produk_id', '=', $request->id_produk)->first();
-        // if ($cek != 0) {
-        //     $cek->
-        // }
         $simpan = Keranjang::create([
             'produk_id' => $request->id_produk,
             'konsumen_id' => $request->session()->get('id_konsumen', 0)
