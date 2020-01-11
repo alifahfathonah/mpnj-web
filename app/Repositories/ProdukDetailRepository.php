e62f0c7862901bf5de6017a06affdc844fa7ec94
LuthfiRobit <luthfirobit@gmail.com>
2020-01-11 16:09:02 +0700

    menambah map function di ProdukDetailRepository

--- app/Repositories/ProdukDetailRepository.php
+++ app/Repositories/ProdukDetailRepository.php
@@ -9,13 +9,16 @@ class ProdukDetailRepository
     public function all()
     {
         return Produk::orderBy('id_produk')
-            ->with('kategori')
+            ->with('kategori', 'foto_produk')
             ->get()
             ->map(
                 function ($produks) {
                     return [
                         'id_produk' => $produks->id_produk,
                         'nama_produk' => $produks->nama_produk,
+                        'id_foto' => $produks->foto_produk->id_foto_produk,
+                        'foto' => $produks->foto_produk->foto_produk,
+                        'id_kategori' => $produks->kategori->id_kategori,
                         'kategori' => $produks->kategori->nama_kategori
                     ];
                 }
@@ -25,7 +28,19 @@ class ProdukDetailRepository
     public function findById($id_produk)
     {
         return Produk::where('id_produk', $id_produk)
-            ->with('kategori')
-            ->firstOrFail();
+            ->with('kategori', 'foto_produk')
+            ->get()
+            ->map(
+                function ($produks) {
+                    return [
+                        'id_produk' => $produks->id_produk,
+                        'nama_produk' => $produks->nama_produk,
+                        'id_foto' => $produks->foto_produk->id_foto_produk,
+                        'foto' => $produks->foto_produk->foto_produk,
+                        'id_kategori' => $produks->kategori->id_kategori,
+                        'kategori' => $produks->kategori->nama_kategori
+                    ];
+                }
+            );
     }
 }
