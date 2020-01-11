5fc09cdf61d83ef5cd2f8bfc008e6152bca78752
LuthfiRobit <luthfirobit@gmail.com>
2020-01-11 16:11:40 +0700

    mengganti routes dari resource ke produkController

--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,17 @@ use App\Http\Resources\DetailProdukResource as DetailProdukResource;
 use App\Models\Produk;
 use App\Models\Kategori_Produk;
 
-//Route::get('/produk', 'Api\DetailProdukController@index');
+//getProdukWithRepository
+Route::get('/produk', 'Api\ProdukController@index');
+
+//getOneProdukWithRepository
+Route::get('/produk/{id_produk}', 'Api\ProdukController@getDetail');
 
 //getAllProdukWithResource
-Route::get('/produk', function () {
-    return ProdukResource::collection(Produk::all());
-});
+// Route::get('/produk', function () {
+//     return ProdukResource::collection(Produk::all());
+// });
 
 Route::get('/kategori', function () {
     return KategoriResource::collection(kategori_Produk::all());
 });
-
-//getOneProdukWithRepository
-Route::get('/produk/{id_produk}', 'Api\DetailProdukController@getDetail');
