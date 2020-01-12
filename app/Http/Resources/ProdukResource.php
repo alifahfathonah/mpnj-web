5e5f51bd97472cb2b909a345d2815630427bdc51
masbro <37744914+masbrodev@users.noreply.github.com>
2020-01-12 16:00:37 +0700

    Revert "Api produk - Menambah Detail By ID dan Relasi dari Pelapak dan Kategori"

--- app/Http/Resources/ProdukResource.php
+++ app/Http/Resources/ProdukResource.php
@@ -19,29 +19,10 @@ class ProdukResource extends JsonResource
             'nama_produk' => $this->nama_produk,
             'harga' => $this->harga_jual,
             'diskon' => $this->diskon,
-            'foto'  => $this->foto,
             'kategori' => new KategoriResource($this->kategori),
             'kategori' => [
                 'id_kategori' => $this->kategori->id_kategori_produk,
                 'nama_kategori' => $this->kategori->nama_kategori,
-            ],
-            'detail' => [
-                'wishlist' => $this->wishlist,
-                'satuan' => $this->satuan,
-                'berat' => $this->berat,
-                'terjual' => $this->terjual,
-                'stok_barang' => $this->stok,
-                'tipe_produk' => $this->tipe_produk,
-                'keterangan' => $this->keterangan,
-            ],
-            'pelapak' => new PelapakResource($this->pelapak),
-            'pelapak' => [
-                'id_pelapak' => $this->pelapak->id_pelapak,
-                'nama_toko' => $this->pelapak->nama_toko,
-                'rating' => $this->pelapak->rating,
-                'status_official' => $this->pelapak->status_official,
-                'alamat' => $this->pelapak->alamat,
-                'status_toko'=> $this->pelapak->status,
             ],
         ];
     }
