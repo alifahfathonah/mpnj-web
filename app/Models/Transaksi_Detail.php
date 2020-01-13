d3b92c25d1c8ebc1e496aa238f042e76dda79433
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-13 16:29:25 +0700

    membuat model transaksi detail

---
+++ app/Models/Transaksi_Detail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi_Detail extends Model
+{
+    protected $table = 'transaksi_detail';
+    protected $primaryKey = 'id_transaksi_detail';
+    protected $fillable = ['transaksi_id','produk_id','jumlah','harga_jual','diskon','kurir','service','ongkir','etd','sub_total','status_order'];
+}
