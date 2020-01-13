d89a9849bc104f2ab6f905d64a6fb5b340b95a6c
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-13 16:28:48 +0700

    membuat tabel transaksi

---
+++ database/migrations/2020_01_13_085806_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->increments('id_transaksi');
+            $table->string('kode_transaksi');
+            $table->integer('konsumen_id')->unsigned();
+            $table->foreign('konsumen_id')->references('id_konsumen')->on('konsumen');
+            $table->dateTime('waktu_transaksi')->useCurrent();
+            $table->integer('total_bayar');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
