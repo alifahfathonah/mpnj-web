d7ddafaec032090fc6eb45ab0ef317617be436c0
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-19 12:14:59 +0700

    membuat KonfirmasiWebController

---
+++ app/Http/Controllers/Web/KonfirmasiWebController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Konfirmasi;
+use App\Models\Transaksi;
+use Illuminate\Http\Request;
+use File;
+
+class KonfirmasiWebController extends Controller
+{
+	protected $kode;
+	
+	public function __construct()
+	{
+		$this->kode = null;
+	}
+	
+    public function index(Request $request)
+    {
+    	return view('web/web_konfirmasi');
+    }
+    
+    public function data(Request $request)
+    {
+    	$kode_transaksi = $request->kode_transaksi;
+    	$cek = Transaksi::with('konsumen')->where('kode_transaksi', $kode_transaksi)->first();
+    	if ($cek != null) {
+//    		$this->kode = $data;
+		    return view('web/web_konfirmasi', ['cek' => $cek]);
+	    } else {
+		    return redirect('konfirmasi');
+	    }
+    }
+    
+    public function simpan(Request $request)
+    {
+    	$foto = $request->file('bukti_transfer');
+    	$filename = $foto->getClientOriginalName();
+    	
+        $simpanKonfirmasi = Konfirmasi::create([
+        	'kode_transaksi' => $request->kode_transaksi,
+	        'total_transfer' => $request->total_bayar,
+	        'rekening_admin_id' => $request->rekening,
+	        'nama_pengirim' => $request->nama_pengirim,
+	        'tanggal_transfer' => date('Y-m-d'),
+	        'bukti_transfer' => $filename
+        ]);
+        
+        if ($simpanKonfirmasi) {
+	        $folder = 'assets/konfirmasi';
+	        $foto->move($folder, $filename);
+	        return 'sukses';
+        }
+    }
+}
