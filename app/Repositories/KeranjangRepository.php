984ee3e37665956db6ada8e0a52fb5265bdd6c19
LuthfiRobit <luthfirobit@gmail.com>
2020-01-20 09:38:58 +0700

    menambah fungsi KeranjangRepository delete updatejumlah chechprice dan minor di all

--- app/Repositories/KeranjangRepository.php
+++ app/Repositories/KeranjangRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Keranjang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KeranjangRepository
 {
@@ -11,7 +12,9 @@ class KeranjangRepository
     {
         return Keranjang::orderBy('id_keranjang')
             ->with('konsumen', 'produk')
+            ->where('status', 'N')
             ->get()
+            // ->get()
             ->map(
                 function ($keranjangs) {
                     return [
@@ -30,11 +33,33 @@ class KeranjangRepository
                         'harga_jual' => $keranjangs->harga_jual
                     ];
                 }
-            );
+            )
+            ->groupBy('produk.pelapak');
     }
 
     public function create($data)
     {
         return Keranjang::create($data);
+    }
+
+    public function delete($id)
+    {
+        return Keranjang::where('id_keranjang', $id)->delete();
+    }
+
+    public function updateJumlah($jumlah, $id)
+    {
+        $keranjang = Keranjang::find($id);
+        $keranjang->jumlah = $jumlah;
+        $updateJumlah = $keranjang->save();
+        if ($updateJumlah) {
+            return $keranjang->jumlah * $keranjang->harga_jual;
+        }
+    }
+
+    public function checkPrice($id_keranjang)
+    {
+        $keranjang = Keranjang::whereIn('id_keranjang', $id_keranjang)->sum(DB::raw('jumlah * harga_jual'));
+        return $keranjang;
     }
 }
