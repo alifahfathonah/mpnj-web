730a54d937ef5728a7417e605a487964c28f91fc
ilham <blogsayailham@gmail.com>
2020-01-24 01:36:59 +0700

    ubah konsumen_id ke pembeli_id dan menambah pembeli_type di transaksi

--- database/migrations/2020_01_13_085806_create_transaksi_table.php
+++ database/migrations/2020_01_13_085806_create_transaksi_table.php
@@ -16,8 +16,9 @@ class CreateTransaksiTable extends Migration
         Schema::create('transaksi', function (Blueprint $table) {
             $table->increments('id_transaksi');
             $table->string('kode_transaksi');
-            $table->integer('konsumen_id')->unsigned();
-            $table->foreign('konsumen_id')->references('id_konsumen')->on('konsumen');
+            $table->integer('pembeli_id');
+            $table->string('pembeli_type');
+//            $table->foreign('konsumen_id')->references('id_konsumen')->on('konsumen');
             $table->dateTime('waktu_transaksi')->useCurrent();
             $table->integer('total_bayar');
             // $table->timestamps();
