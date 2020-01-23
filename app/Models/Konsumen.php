cc55506c926902c3e2443ef01c1914da957c0cc5
ilham <blogsayailham@gmail.com>
2020-01-24 01:39:46 +0700

    extends Authenticatable di model konsumen

--- app/Models/Konsumen.php
+++ app/Models/Konsumen.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Konsumen extends Model
+class Konsumen extends Authenticatable
 {
     protected $table = 'konsumen';
     protected $primaryKey = 'id_konsumen';
