78968ec44ce13751cc088d8e4d36f72e674fbc05
ilham <blogsayailham@gmail.com>
2020-01-24 01:41:24 +0700

    notifiable, guard, hidden dan polymorphic relasi di model pelapak

--- app/Models/Pelapak.php
+++ app/Models/Pelapak.php
@@ -3,10 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Pelapak extends Authenticatable
 {
+	use Notifiable;
+	
+	protected $guard = 'pelapak';
+	
     protected $table = 'pelapak';
     protected $primaryKey = 'id_pelapak';
     protected $fillable = ['nama_lengkap','username','password','provinsi_id','city_id','alamat','kode_pos','email','nomor_hp','status','nama_toko','alamat_toko','status_official'];
+	protected $hidden = ['password'];
+
+    public function transaksi()
+    {
+        return $this->morphOne(Transaksi::class, 'pembeli');
+    }
+
+    public function keranjang()
+    {
+        return $this->morphOne(Keranjang::class, 'pembeli');
+    }
 }
