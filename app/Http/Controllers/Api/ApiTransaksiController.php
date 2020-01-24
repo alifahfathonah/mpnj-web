f9fbd6c525cbc564089b8578df4bf9491cac5cad
LuthfiRobit <luthfirobit@gmail.com>
2020-01-25 02:12:39 +0700

    menambah fungsi simpan di ApiTransaksiController

--- app/Http/Controllers/Api/ApiTransaksiController.php
+++ app/Http/Controllers/Api/ApiTransaksiController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaksi;
+use App\Models\Transaksi_Detail;
 use App\Repositories\TransaksiRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ApiTransaksiController extends Controller
 {
@@ -19,5 +22,37 @@ class ApiTransaksiController extends Controller
     {
         $transaksi = $this->transaksiRepository->all();
         return $transaksi;
+    }
+
+    public function simpan(Request $request)
+    {
+        $role = Session::get('role');
+        //$id = Session::get('id');
+        //$konsumen_id = $request->user($role)->$id;
+
+        $data = array(
+            'pembeli_id' => $request->pembeli_id,
+            'pembeli_type' => $request->$role == 'konsumen' ? 'App\Models\Konsumen' : 'App\Models\Pelapak',
+            'kode_transaksi' => time(),
+            'waktu_transaksi' => date('Y-m-d H:i:s'),
+            'total_bayar' => $request->total_bayar
+        );
+
+        $transaksi = $this->transaksiRepository->create($data);
+        if ($transaksi) {
+            // foreach ($request->trxDetail as $detail) {
+            //     $detail['transaksi_id'] = $transaksi->id_transaksi;
+            //     Transaksi_Detail::create($detail);
+            // }
+            return response()->json(
+                [
+                    'kode_transaksi' => $transaksi->kode_transaksi,
+                    'total_bayar' => $transaksi->total_bayar
+                ],
+                200
+            );
+        } else {
+            return response()->json('gagal', 400);
+        }
     }
 }
