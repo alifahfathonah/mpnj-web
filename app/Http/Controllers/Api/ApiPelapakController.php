ede212455b42f7e80e46b8c5f736e7c5d6138451
Masbro <s2gotosingapore@gmail.com>
2020-01-25 01:57:31 +0700

    Menambah Fungsi Tambah Api Pelapak

--- app/Http/Controllers/Api/ApiPelapakController.php
+++ app/Http/Controllers/Api/ApiPelapakController.php
@@ -30,4 +30,25 @@ class ApiPelapakController extends Controller
     {
         return new PelapakResource($id_pelapak);
     }
+
+    public function create(request $request)
+    {
+            $pelapak = new Pelapak;
+            $pelapak->username = $request->username;
+            $pelapak->password = $request->password;
+            $pelapak->status_official = $request->status_official;
+            $pelapak->nama_toko = $request->nama_toko;
+            $pelapak->alamat_toko = $request->alamat_toko;
+            $pelapak->provinsi_id = $request->provinsi_id;
+            $pelapak->city_id = $request->city_id;
+            $pelapak->alamat = $request->alamat;
+            $pelapak->kode_pos = $request->kode_pos;
+            $pelapak->nomor_hp = $request->nomor_hp;
+            $pelapak->email = $request->email;
+            $pelapak->rating = $request->rating;
+            $pelapak->saldo = $request->saldo;
+            $pelapak->status = $request->status;
+
+            $pelapak->save();
+    }
 }
