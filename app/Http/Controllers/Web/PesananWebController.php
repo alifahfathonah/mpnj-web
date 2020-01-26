7524512be8b3c7e0000fbc4debedafb24de91e27
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-26 23:07:24 +0700

    membuat fungsi index di pesananWebController

---
+++ app/Http/Controllers/Web/PesananWebController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Konsumen;
+use App\Models\Pelapak;
+use App\Models\Transaksi;
+use App\Models\Transaksi_Detail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class PesananWebController extends Controller
+{
+    public function index(Request $request)
+    {
+        $role = Session::get('role');
+        $id = Session::get('id');
+        $konsumen_id = $request->user($role)->$id;
+
+        $data['order'] = Transaksi::with('pembeli')
+                        ->where('pembeli_id', $konsumen_id)
+                        ->where('pembeli_type', $role == 'konsumen' ? Konsumen::class : Pelapak::class)
+                        ->get();
+        return view('web/web_pesanan', $data);
+    }
