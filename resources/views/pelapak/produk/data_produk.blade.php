a57d142bf960d5c35c429cab300f825ffd1e64f1
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-28 00:38:55 +0700

    perubahan minor tampilan data produk pelapak

--- resources/views/pelapak/produk/data_produk.blade.php
+++ resources/views/pelapak/produk/data_produk.blade.php
@@ -1,53 +1,164 @@
-@extends('pelapak.master')
+@extends('web.web_master')
 
-@section('title')
-    Data Produk
-@endsection
-
-@section('konten')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card card-primary card-outline">
-                <div class="card-header">
-                    <h3 class="card-title">
-                        Data Produk
-                    </h3>
+@section('web_konten')
+    <!--================================
+        START BREADCRUMB AREA
+    =================================-->
+    <section class="breadcrumb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="breadcrumb">
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">Pelapak</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">Produk</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <h1 class="page-title">Data Produk</h1>
                 </div>
-                <div class="card-body pad table-responsive">
-                    <a href="/administrator/produk/tambah" class="btn btn-primary btn-sm">Tambah</a> <br> <br>
-                    <table id="tbl" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>NO</th>
-                                <th>Nama Produk</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($produk as $p)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $p->nama_produk }}</td>
-                                    <td>
-                                        <a href="/administrator/produk/edit/{{ $p->id_produk }}" class="btn btn-primary btn-sm">Edit</a>
-                                        <a href="/administrator/produk/hapus/{{ $p->id_produk }}" class="btn btn-danger btn-sm">Hapus</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                <!-- end /.col-md-12 -->
             </div>
+            <!-- end /.row -->
         </div>
-    </div>
-</div>
-@endsection
+        <!-- end /.container -->
+    </section>
+    <!--================================
+            END BREADCRUMB AREA
+        =================================-->
 
-@push('scripts')
-    <script>
-        $(function() {
-            $("#tbl").DataTable();
-        })
-    </script>
-@endpush
+    <!--================================
+                START DASHBOARD AREA
+        =================================-->
+    <section class="dashboard-area">
+        @include('pelapak.master')
+        <!-- end /.dashboard_menu_area -->
+
+        <div class="dashboard_contents">
+            <div class="container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="filter-bar dashboard_title_area clearfix filter-bar2">
+                                <div class="dashboard__title dashboard__title pull-left">
+                                    <h3>Data Produk</h3>
+                                </div>
+
+                                <div class="pull-right">
+                                    <div class="filter__option filter--text">
+                                        <p>
+                                            <span>26</span> Products</p>
+                                    </div>
+
+                                    <div class="filter__option filter--select">
+                                        <div class="select-wrap">
+                                            <select name="price">
+                                                <option value="low">Price : Low to High</option>
+                                                <option value="high">Price : High to low</option>
+                                            </select>
+                                            <span class="lnr lnr-chevron-down"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end /.pull-right -->
+                            </div>
+                            <!-- end /.filter-bar -->
+                        </div>
+                        <!-- end /.col-md-12 -->
+                    </div>
+
+                    <div class="row">
+                        @foreach($produk as $p)
+                            <div class="col-lg-4 col-md-6">
+                                <!-- start .single-product -->
+                                <div class="product product--card">
+
+                                    <div class="product__thumbnail">
+                                        <img src="{{ asset('assets/foto_produk/'.$p->foto_produk[0]->foto_produk) }}" alt="Product Image" width="361" height="230">
+
+                                        <div class="prod_option">
+                                            <a href="#" id="drop4" class="dropdown-trigger" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                <span class="lnr lnr-cog setting-icon"></span>
+                                            </a>
+
+                                            <div class="options dropdown-menu" aria-labelledby="drop4">
+                                                <ul>
+                                                    <li>
+                                                        <a href="edit-item.html">
+                                                            <span class="lnr lnr-pencil"></span>Edit</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">
+                                                            <span class="lnr lnr-eye"></span>Hide</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#" data-toggle="modal" data-target="#myModal2" class="delete">
+                                                            <span class="lnr lnr-trash"></span>Delete</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- end /.product__thumbnail -->
+
+                                    <div class="product-desc">
+                                        <a href="#" class="product_title">
+                                            <h4>{{ $p->nama_produk }}</h4>
+                                        </a>
+                                        <ul class="titlebtm">
+                                            <li>
+                                                <img class="auth-img" src="{{ asset('assets/images/auth3.jpg') }}" alt="author image">
+                                                <p>
+                                                    <a href="#">{{ $p->pelapak->nama_toko }}</a>
+                                                </p>
+                                            </li>
+{{--                                            <li class="product_cat">--}}
+{{--                                                <a href="#">--}}
+{{--                                                    <span class="lnr lnr-book"></span>Plugin</a>--}}
+{{--                                            </li>--}}
+                                        </ul>
+                                    </div>
+                                    <!-- end /.product-desc -->
+
+                                    <div class="product-purchase">
+                                        <a href="#">
+                                            <div class="price_love">
+                                                <span>Edit</span>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="price_love">
+                                                <span>Hapus</span>
+                                            </div>
+                                        </a>
+
+                                        <div class="sell">
+                                            <p>
+                                                <span class="lnr lnr-cart"></span>
+                                                <span>27</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <!-- end /.product-purchase -->
+                                </div>
+                                <!-- end /.single-product -->
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <!-- end /.row -->
+            </div>
+            <!-- end /.container -->
+        </div>
+        <!-- end /.dashboard_menu_area -->
+    </section>
+    <!--================================
+            END DASHBOARD AREA
+    =================================-->
+@endsection
