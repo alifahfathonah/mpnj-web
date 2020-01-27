f726ad9b1bac16a53cb23edc88dae7dcf09b22bd
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-01-28 00:38:36 +0700

    membuat file home untuk pelapak

---
+++ resources/views/pelapak/home.blade.php
@@ -0,0 +1,54 @@
+@extends('web.web_master')
+
+@section('web_konten')
+    <!--================================
+        START BREADCRUMB AREA
+    =================================-->
+    <section class="breadcrumb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="breadcrumb">
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">Pelapak</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <h1 class="page-title">Mulailah Menjual</h1>
+                </div>
+                <!-- end /.col-md-12 -->
+            </div>
+            <!-- end /.row -->
+        </div>
+        <!-- end /.container -->
+    </section>
+    <!--================================
+            END BREADCRUMB AREA
+        =================================-->
+
+    <!--================================
+            START DASHBOARD AREA
+    =================================-->
+    <section class="dashboard-area">
+        @include('pelapak.master')
+        <!-- end /.dashboard_menu_area -->
+
+        <div class="dashboard_contents">
+            <div class="container">
+                <div class="container">
+                    <h2>DASHBOARD</h2>
+                </div>
+                <!-- end /.row -->
+            </div>
+            <!-- end /.container -->
+        </div>
+        <!-- end /.dashboard_menu_area -->
+    </section>
+    <!--================================
+            END DASHBOARD AREA
+    =================================-->
+@endsection
