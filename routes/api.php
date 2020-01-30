53b167bd491c62a0d9ff6a05452091924a829dd7
LuthfiRobit <luthfirobit@gmail.com>
2020-01-30 20:09:04 +0700

    menambah routing post untuk apikonfirmasicontroller

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Resources\DetailProdukResource as DetailProdukResource;
 use App\Models\Produk;
 use App\Models\Kategori_Produk;
 // use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 //api produk dengan resource
 Route::get('/produk', 'Api\ApiProdukController@index');
@@ -49,3 +49,4 @@ Route::post('/transaksi/simpan', 'Api\ApiTransaksiController@simpan');
 
 //konfirmasi
 Route::get('/konfirmasi/{kode_transaksi}', 'Api\ApiKonfirmasiController@tampilData');
+Route::post('/konfirmasi/simpan', 'Api\ApiKonfirmasiController@simpan');
