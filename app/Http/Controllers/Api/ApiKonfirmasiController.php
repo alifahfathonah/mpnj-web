70862d37e67d724846e9201b4fe19411fca6cf39
LuthfiRobit <luthfirobit@gmail.com>
2020-01-30 20:08:31 +0700

    menambah fungsi simpan di apikonfirmasicontroller

--- app/Http/Controllers/Api/ApiKonfirmasiController.php
+++ app/Http/Controllers/Api/ApiKonfirmasiController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Konfirmasi;
 use App\Models\Transaksi;
 use App\Repositories\KonfirmasiRepository;
 use Illuminate\Http\Request;
+use File;
 
 class ApiKonfirmasiController extends Controller
 {
@@ -30,5 +32,33 @@ class ApiKonfirmasiController extends Controller
             $res2['data'] = [];
             return response()->json($res2);
         };
+    }
+
+    public function simpan(Request $request)
+    {
+        // return $request->kode_transaksi;
+        $konfirm = new Konfirmasi;
+        $konfirm->kode_transaksi = $request->kode_transaksi;
+        $konfirm->total_transfer = $request->total_transfer;
+        $konfirm->rekening_admin_id = $request->rekening_admin_id;
+        $konfirm->nama_pengirim = $request->nama_pengirim;
+        $konfirm->tanggal_transfer = date('Y-m-d');
+        // $file = $request->file('bukti_transfer');
+        // $name = uniqid() . '_' . trim($file->getClientOriginalName());
+        // $file->move('assets/foto_bukti_tf', $name);
+        // // $file = $konfirm['base64_image'];
+        // $konfirm->bukti_transfer = $name;
+        $simpan = $konfirm->save();
+
+        if ($simpan) {
+            // $file->move($file);
+            $res['pesan'] = "Tambah Data Produk Sukses!";
+            $res['data'] = [$konfirm];
+
+            return response()->json($res, 201);
+        } else {
+            $res2['pesan'] = "Tambah Data produk Gagal!";
+            return response()->json($res2);
+        }
     }
 }
