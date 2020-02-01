b92788098b907a52f0b78a7a5ed974fc4d0ce5d6
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-01 19:04:21 +0700

    perubahan query data transaksi untuk pelapak (temporary)

--- app/Http/Controllers/Pelapak/PelapakTransaksiController.php
+++ app/Http/Controllers/Pelapak/PelapakTransaksiController.php
@@ -15,12 +15,18 @@ class PelapakTransaksiController extends Controller
         $role = Session::get('role');
         $id = Session::get('id');
         $konsumen_id = $request->user($role)->$id;
-
-        $data['transaksi'] = Transaksi::with(['transaksi_detail' => function($q) use ($konsumen_id) {
-            $q->where('pelapak_id', $konsumen_id);
-        }])->get();
+//
+//        $data['transaksi'] = Transaksi::with(['transaksi_detail' => function($query) {
+//            $query->whereExist('pelapak_id', 1);
+//        }])->get();
+//        $data = Transaksi::join('transaksi_detail', 'transaksi_detail.transaksi_id', '=', 'transaksi.id_transaksi')
+//                ->where('transaksi_detail.pelapak_id', 1)->distinct('kode_transaksi')->get();
+        $data['transaksi'] = DB::table('transaksi')->distinct()
+                ->join('transaksi_detail', 'transaksi_detail.transaksi_id', '=', 'transaksi.id_transaksi')
+                ->where('transaksi_detail.pelapak_id', $konsumen_id)
+                ->get(['id_transaksi','kode_transaksi', 'waktu_transaksi','total_bayar']);
         return view('pelapak/transaksi/data_transaksi', $data);
-//        return $data['transaksi'];
+//        return $data;
     }
 
     public function detail(Request $request, $id)
