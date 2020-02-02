0621b8e8233c8f63a75c734c24ce3e70416d7712
Masbro <s2gotosingapore@gmail.com>
2020-02-02 23:27:22 +0700

    Import Fungsi Hash

--- app/Http/Controllers/Api/ApiRegisterKonsumenController.php
+++ app/Http/Controllers/Api/ApiRegisterKonsumenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Konsumen;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ApiRegisterKonsumenController extends Controller
 {
