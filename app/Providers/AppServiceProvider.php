cce6f7037af42bf02dd08d3df706757a88835192
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-02 21:59:30 +0700

    membuat fungsi untuk data for all views di AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,22 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('currency', function($expression) {
             return "Rp. <?php echo number_format($expression, 0, ', ', '.'); ?>";
         });
+
+        view()->composer('*', function ($view)
+        {
+            $role = Session::get('role');
+            $id = Session::get('id');
+            $konsumen_id = Auth::guard($role)->user()->$id;
+
+            $keranjang = Keranjang::with(['produk', 'pembeli'])
+                ->where('pembeli_id', $konsumen_id)
+                ->where('pembeli_type', $role == 'konsumen' ? 'App\Models\Konsumen' : 'App\Models\Pelapak')
+                ->where('status', 'N')
+                ->limit(1)
+                ->first();
+
+            //...with this variable
+            $view->with('cart', $keranjang );
+        });
     }
 }
