96bbfc3719365613fb8db6f05da36c3265a9dc78
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-05 03:26:23 +0700

    membuat fungsi simpan alamat

--- app/Http/Controllers/Web/ProfileWebController.php
+++ app/Http/Controllers/Web/ProfileWebController.php
@@ -61,5 +61,29 @@ class ProfileWebController extends Controller
 
         return view('web/web_profile', $data);
     }
+    public function simpan_alamat(Request $request)
+    {
+        $role = Session::get('role');
+        $sessionId = Session::get('id');
+        $user_id = Auth::guard($role)->user()->$sessionId;
+
+        $data = [
+          'nama' => $request->nama,
+          'nomor_telepon' => $request->nomor_telepon,
+          'provinsi_id' => $request->provinsi,
+          'nama_provinsi' => $request->nama_provinsi,
+          'city_id' => $request->kota,
+          'nama_kota' => $request->nama_kota,
+          'kode_pos' => $request->kode_pos,
+          'kecamatan_id' => 0,
+          'alamat_lengkap' => $request->alamat_lengkap,
+          'user_id' => $user_id,
+          'user_type' => $role == 'konsumen' ? 'App\Models\Konsumen' : 'App\Models\Pelapak'
+        ];
+
+        $simpan = Alamat::create($data);
+        if ($simpan) {
+            return redirect()->back();
+        }
     }
 }
