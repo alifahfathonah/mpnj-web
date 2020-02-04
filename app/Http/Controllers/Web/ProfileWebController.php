fe5a3186d988f5a27439428078942df44e829f88
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-05 03:22:01 +0700

    initialize guzzle in profile web controller

--- app/Http/Controllers/Web/ProfileWebController.php
+++ app/Http/Controllers/Web/ProfileWebController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Alamat;
 use App\Models\Konsumen;
 use App\Models\Pelapak;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
 class ProfileWebController extends Controller
 {
+    protected $client, $token;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->token = env('API_RAJAONGKIR');
+    }
     public function index()
     {
         return view('web/web_profile');
