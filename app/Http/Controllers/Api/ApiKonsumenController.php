2650bc6dcabf6a0fa45a5fe513f01549370cea68
Masbro <s2gotosingapore@gmail.com>
2020-02-06 15:12:42 +0700

    Menambah Function Lupa Password

--- app/Http/Controllers/Api/ApiKonsumenController.php
+++ app/Http/Controllers/Api/ApiKonsumenController.php
@@ -8,5 +8,25 @@ use DB;
 
 class ApiKonsumenController extends Controller
 {
+     public function lupa_password(Request $request, $kosumenId)
+    {
+        
+        $konsumen = Konsumen::find($kosumenId);
+        $konsumen->password = Hash::make(Request::get('password'));
+
+        if($request->fails()){
+            $res ['pesan'] = "gagal";
+            $res ['response'] = $request->messages();
+
+            return response()->json($res);
+        }else{
+            $konsumen->save();
+            $res ['data'] = [$konsumen];
+            $res2 ['pesan'] = "Sukses!";
+            $res2 ['data'] = [$konsumen];
+            
+            return response()->json($res2);
+        }
+    }
 
 }
