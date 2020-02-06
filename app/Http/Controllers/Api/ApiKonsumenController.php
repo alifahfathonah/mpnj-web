95980cb3544ad8751ee6bc5f6219ad93576de082
Masbro <s2gotosingapore@gmail.com>
2020-02-06 15:11:40 +0700

    Tambah Controller APIKonsumen

---
+++ app/Http/Controllers/Api/ApiKonsumenController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+use Request;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class ApiKonsumenController extends Controller
+{
+
+}
