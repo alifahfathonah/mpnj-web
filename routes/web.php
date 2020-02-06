8366f3e40871830897f24ff8b523d71b39885f2c
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-06 13:33:32 +0700

    perubahan minor routing profile/rekening

--- routes/web.php
+++ routes/web.php
@@ -76,9 +76,9 @@ Route::get('pesanan', 'Web\PesananWebController@index');
 Route::get('pesanan/detail/{id}', 'Web\PesananWebController@detail');
 
 //konsumen profile
-Route::get('profile', 'Web\ProfileWebController@index');
 Route::get('profile', 'Web\ProfileWebController@index')->name('profile');
 Route::post('profile/ubah/{role}/{id}', 'Web\ProfileWebController@ubah');
+Route::get('profile/rekening', 'Web\ProfileWebController@rekening')->name('rekening');
 Route::get('profile/alamat', 'Web\ProfileWebController@alamat')->name('alamat');
 Route::post('profile/alamat/simpan', 'Web\ProfileWebController@simpan_alamat');
 Route::post('profile/alamat/ubah/{id}', 'Web\ProfileWebController@ubah_alamat');
