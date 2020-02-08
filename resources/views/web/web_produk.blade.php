27ed15c27ace1c0db65fd4bb04fce0cfc0b29e5f
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-09 00:54:09 +0700

    refactor halaman daftar produk + paginasi

--- resources/views/web/web_produk.blade.php
+++ resources/views/web/web_produk.blade.php
@@ -1,16 +1,206 @@
 @extends('web.web_master')
 
 @section('web_konten')
-@foreach ($produk as $p)
-<div class="row">
-    <div class="card" style="width: 18rem;">
-        <img src="http://localhost:8000/assets/foto_produk/{{ $p->foto_produk[0]->foto_produk }}" class="card-img-top" alt="{{ $p->nama_Produk }}">
-        <div class="card-body">
-            <h5 class="card-title">{{ $p->nama_produk }}</h5>
-            <p class="card-text">{{ $p->keterangan }}</p>
-            <a href="/produk/{{ $p->id_produk }}" class="btn btn-primary">Detail</a>
+    <section class="search-wrapper">
+        <div class="search-area2 bgimage">
+            <div class="bg_image_holder" style="background-image: url({{ asset('assets/images/search.jpg') }}); opacity: 1;">
+                <img src="images/search.jpg" alt="images/search.jpg">
+            </div>
+            <div class="container content_above">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <div class="search">
+                            <div class="search__title">
+                                <h3>
+                                    <span>35,270</span> website templates from our creative community</h3>
+                            </div>
+                            <div class="search__field">
+                                <form action="#">
+                                    <div class="field-wrapper">
+                                        <input class="relative-field rounded" type="text" placeholder="Search your products">
+                                        <button class="btn btn--round" type="submit">Search</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="breadcrumb">
+                                <ul>
+                                    <li>
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li class="active">
+                                        <a href="#">All Products</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end /.row -->
+            </div>
+            <!-- end /.container -->
+        </div>
+        <!-- end /.search-area2 -->
+    </section>
+
+    <div class="filter-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="filter-bar filter--bar2">
+                        <div class="pull-right">
+                            <div class="filter__option filter--text pull-left">
+                                <a href="#">New Products</a>
+                                <a href="#">Popular Products</a>
+                            </div>
+                            <div class="filter__option filter--select">
+                                <div class="select-wrap">
+                                    <select name="price">
+                                        <option value="low">Price : Low to High</option>
+                                        <option value="high">Price : High to low</option>
+                                    </select>
+                                    <span class="lnr lnr-chevron-down"></span>
+                                </div>
+                            </div>
+                            <div class="filter__option filter--select">
+                                <div class="select-wrap">
+                                    <select name="price">
+                                        <option value="12">12 Items per page</option>
+                                        <option value="15">15 Items per page</option>
+                                        <option value="25">25 Items per page</option>
+                                    </select>
+                                    <span class="lnr lnr-chevron-down"></span>
+                                </div>
+                            </div>
+                            <div class="filter__option filter--layout">
+                                <a href="category-grid.html">
+                                    <div class="svg-icon">
+                                        <img class="svg" src="{{ asset('assets/images/svg/grid.svg') }}" alt="it's just a layout control folks!">
+                                    </div>
+                                </a>
+                                <a href="category-list.html">
+                                    <div class="svg-icon">
+                                        <img class="svg" src="{{ asset('assets/images/svg/list.svg') }}" alt="it's just a layout control folks!">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end filter-bar -->
+                </div>
+                <!-- end /.col-md-12 -->
+            </div>
+            <!-- end filter-bar -->
         </div>
     </div>
-</div>
-@endforeach
+
+    <section class="products section--padding2">
+        <!-- start container -->
+        <div class="container">
+            <!-- start .row -->
+            <div class="row">
+                <!-- start .col-md-3 -->
+                <div class="col-lg-3">
+                    <!-- start aside -->
+                    <aside class="sidebar product--sidebar">
+                        <div class="sidebar-card card--category">
+                            <a class="card-title" href="#collapse1" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapse1">
+                                <h4>Kategori
+                                    <span class="lnr lnr-chevron-down"></span>
+                                </h4>
+                            </a>
+                            <div class="collapse show collapsible-content" id="collapse1">
+                                <ul class="card-content">
+                                    @foreach($kategori as $k)
+                                        <li>
+                                            <a href="{{ URL::to('produk?kategori='.strtolower($k->nama_kategori)) }}">
+                                                <span class="lnr lnr-chevron-right"></span>{{ $k->nama_kategori }}
+                                                <span class="item-count">0</span>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <!-- end /.collapsible_content -->
+                        </div>
+                        <!-- end /.sidebar-card -->
+                    </aside>
+                    <!-- end aside -->
+                </div>
+                <!-- end /.col-md-3 -->
+
+                <!-- start col-md-9 -->
+                <div class="col-lg-9">
+                    <div class="row">
+                        @foreach($produk as $p)
+                            <div class="col-lg-4 col-md-6">
+                                <!-- start .single-product -->
+                                <div class="product product--card product--card-small">
+
+                                    <div class="product__thumbnail">
+                                        <img src="{{ asset('assets/foto_produk/'.$p->foto_produk[0]->foto_produk) }}" alt="Product Image" height="270px">
+                                        <div class="prod_btn">
+                                            <a href="{{ URL::to('produk/'.$p->id_produk) }}" class="transparent btn--sm btn--round">Detail</a>
+                                        </div>
+                                        <!-- end /.prod_btn -->
+                                    </div>
+                                    <!-- end /.product__thumbnail -->
+
+                                    <div class="product-desc">
+                                        <a href="{{ URL::to('produk/'.$p->id_produk) }}" class="product_title">
+                                            <p>{{ $p->nama_produk }}</p>
+                                        </a>
+                                    </div>
+                                    <!-- end /.product-desc -->
+
+                                    <div class="product-purchase">
+                                        <div class="price_love">
+                                            <span>@currency($p->harga_jual)</span>
+                                        </div>
+                                        <a href="{{ URL::to('produk?kategori='.strtolower($p->kategori->nama_kategori)) }}">
+                                            <span class="lnr lnr-book"></span>{{ $p->kategori->nama_kategori }}</a>
+                                    </div>
+                                    <!-- end /.product-purchase -->
+                                </div>
+                                <!-- end /.single-product -->
+                            </div>
+                        @endforeach
+                        <!-- end /.col-md-4 -->
+
+                    </div>
+                    {{ $produk->links() }}
+                </div>
+                <!-- end /.col-md-9 -->
+            </div>
+            <!-- end /.row -->
+
+{{--            <div class="row">--}}
+{{--                <div class="col-md-12">--}}
+{{--                    <div class="pagination-area categorised_item_pagination">--}}
+{{--                        <nav class="navigation pagination" role="navigation">--}}
+{{--                            @if($produk->lastPage() > 1)--}}
+{{--                                <div class="nav-links page-number">--}}
+{{--                                    @if($produk->currentPage() != $produk->onFirstPage())--}}
+{{--                                        <a class="prev page-numbers" href="{{ $produk->previousPageUrl() }}">--}}
+{{--                                            <span class="lnr lnr-arrow-left"></span>--}}
+{{--                                        </a>--}}
+{{--                                    @endif--}}
+{{--                                    @for($i = 1; $i <= $produk->lastPage(); $i++)--}}
+{{--                                        <a class="page-numbers {{ $i == $produk->currentPage() ? 'current' : '' }}" href="{{ $produk->url($i) }}">{{ $i }}</a>--}}
+{{--                                    @endfor--}}
+{{--                                    @if($produk->currentPage() != $produk->lastPage())--}}
+{{--                                        <a class="next page-numbers" href="{{ $produk->nextPageUrl()  }}">--}}
+{{--                                            <span class="lnr lnr-arrow-right"></span>--}}
+{{--                                        </a>--}}
+{{--                                    @endif--}}
+{{--                                </div>--}}
+{{--                            @endif--}}
+{{--                        </nav>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <!-- end /.row -->
+        </div>
+        <!-- end /.container -->
+
+    </section>
 @endsection
