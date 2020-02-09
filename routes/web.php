62d43a3287b8375cb6e6abf0c184dbb7146540cf
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-10 03:12:39 +0700

    routing get halaman pelapak

--- routes/web.php
+++ routes/web.php
@@ -87,5 +87,7 @@ Route::get('profile/alamat/ubah/utama/{id}', 'Web\ProfileWebController@ubah_alam
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+//web pelapak
+Route::get('pelapak/{username}', 'Web\PelapakWebController@index')->name('halaman_pelapak');
 //pelapak
 Route::get('jual', 'Pelapak\PelapakController@index');
