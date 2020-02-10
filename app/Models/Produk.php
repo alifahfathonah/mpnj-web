3de2c4d320b1cdc9ac88a7a2eb0db94437ac2020
Masbro <s2gotosingapore@gmail.com>
2020-02-11 06:46:45 +0700

    Slug Function

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -11,6 +11,10 @@ class Produk extends Model
     protected $primaryKey = 'id_produk';
     protected $fillable = ['nama_produk','satuan','berat','harga_modal','harga_jual','diskon','stok','keterangan','tipe_produk','foto','wishlist','terjual','pelapak_id','kategori_produk_id', 'slug'];
 
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = Str::slug($value); 
+    }
 
     public function foto_produk()
     {
