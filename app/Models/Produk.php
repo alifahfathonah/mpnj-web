a6416c530050974fc9c58590b5657338c210b39b
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-13 00:16:23 +0700

    hapus fungsi setSlugAttribute

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -11,11 +11,6 @@ class Produk extends Model
     protected $primaryKey = 'id_produk';
     protected $fillable = ['nama_produk','satuan','berat','harga_modal','harga_jual','diskon','stok','keterangan','tipe_produk','foto','wishlist','terjual','pelapak_id','kategori_produk_id', 'slug'];
 
-    public function setSlugAttribute($value)
-    {
-        $this->attributes['slug'] = Str::slug($value); 
-    }
-
     public function foto_produk()
     {
         return $this->hasMany(Foto_Produk::class, 'produk_id', 'id_produk');
