5d2b8a0513476050e48998d7f83bffcd578c187e
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2020-02-13 12:01:55 +0700

    hapus gateway rajaongkir di index konsumen web controller

--- app/Http/Controllers/Web/KonsumenWebController.php
+++ app/Http/Controllers/Web/KonsumenWebController.php
@@ -9,23 +9,9 @@ use Illuminate\Http\Request;
 
 class KonsumenWebController extends Controller
 {
-    protected $client, $token;
-
-    public function __construct()
-    {
-        $this->client = new Client();
-        $this->token = env('API_RAJAONGKIR');
-    }
-
     public function index()
     {
-        $request = $this->client->get('https://api.rajaongkir.com/starter/province', [
-            'headers' => [
-                'key' => $this->token
-            ]
-        ])->getBody()->getContents();
-        $data['provinsi'] = json_decode($request, false);
-        return view('auth/register', $data);
+        return view('auth/register');
     }
 
     public function simpan(Request $request)
