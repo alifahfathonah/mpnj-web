7bc606d6584609534bced3408f4993cfc031458d
Masbro <s2gotosingapore@gmail.com>
2020-02-24 00:16:31 +0700

    Import File

--- app/Http/Controllers/Api/ApiRegisterKonsumenController.php
+++ app/Http/Controllers/Api/ApiRegisterKonsumenController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Konsumen;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use File;
 
 class ApiRegisterKonsumenController extends Controller
 {
