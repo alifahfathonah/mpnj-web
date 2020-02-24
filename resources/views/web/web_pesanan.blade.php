48ed6db7a1c7fbbbd0353fd89a6296d7d0846af3
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-25 00:12:38 +0700

    menambah data tab untuk semua transaksi

--- resources/views/web/web_pesanan.blade.php
+++ resources/views/web/web_pesanan.blade.php
@@ -64,7 +64,143 @@
                     </div>
 
                     <div class="tab-content table-responsive">
-                        <div class="fade show tab-pane product-tab active" id="pending">
+                        <div class="fade show tab-pane product-tab active" id="semua">
+                            <div class="tab-content-wrapper">
+                                <div class="product_archive table-responsive">
+                                    <div class="title_area">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <h4>Detail Produk</h4>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <h4 class="add_info">Informasi Tambahan</h4>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <h4>Total</h4>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <h4>Action</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        @foreach($order as $key => $v)
+                                            @foreach($order[$key] as $val)
+                                                <div class="col-md-12">
+                                                    <div class="single_product clearfix">
+                                                        <div class="row">
+                                                            <div class="col-lg-5 col-md-5">
+                                                                <div class="product__description">
+                                                                    <img src="{{ asset('assets/foto_produk/'.$val->produk->foto_produk[0]->foto_produk) }}" alt="{{ $val->produk->nama_produk }}" height="100px">
+                                                                    <div class="short_desc">
+                                                                        <h4>{{ $val->produk->nama_produk }}</h4>
+                                                                        <p>
+                                                                            Harga :
+                                                                            @if($val->diskon == 0)
+                                                                                @currency($val->harga_jual)
+                                                                            @else
+                                                                                <strike style="color: red">@currency($val->harga_jual)</strike> | @currency($val->harga_jual - ($val->diskon / 100 * $val->harga_jual))
+                                                                            @endif
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                                <!-- end /.product__description -->
+                                                            </div>
+                                                            <!-- end /.col-md-5 -->
+
+                                                            <div class="col-lg-3 col-md-3 col-6 xs-fullwidth">
+                                                                <div class="product__additional_info">
+                                                                    <ul>
+                                                                        {{--                                                                            <li>--}}
+                                                                        {{--                                                                                <p>--}}
+                                                                        {{--                                                                                    <span>Tanggal: </span> {{ $val->transaksi->waktu_transaksi }}--}}
+                                                                        {{--                                                                                </p>--}}
+                                                                        {{--                                                                            </li>--}}
+                                                                        <li class="license">
+                                                                            <p>
+                                                                                <span>Jumlah:</span>  {{ $val->jumlah }}
+                                                                            </p>
+                                                                        </li>
+                                                                        <li>
+                                                                            <p>
+                                                                                <span>Kurir:</span>  {{ $val->kurir }}
+                                                                            </p>
+                                                                        </li>
+                                                                        <li>
+                                                                            <p>
+                                                                                <span>Service:</span>  {{ $val->service }}
+                                                                            </p>
+                                                                        </li>
+                                                                        <li>
+                                                                            <p>
+                                                                                <span>Ongkir:</span>  @currency($val->ongkir)
+                                                                            </p>
+                                                                        </li>
+                                                                        <li>
+                                                                            <p>
+                                                                                <a href="{{ URL::to('pelapak/'.$val->produk->pelapak->username) }}"><span>Penjual:</span> {{ $val->produk->pelapak->nama_toko }}</a>
+                                                                            </p>
+                                                                        </li>
+                                                                        {{--                                                                            <li>--}}
+                                                                        {{--                                                                                <a href="#">--}}
+                                                                        {{--                                                                                    <img src="images/catword.png" alt="">Wordpress</a>--}}
+                                                                        {{--                                                                            </li>--}}
+                                                                    </ul>
+                                                                </div>
+                                                                <!-- end /.product__additional_info -->
+                                                            </div>
+                                                            <!-- end /.col-md-3 -->
+
+                                                            <div class="col-lg-4 col-md-4 col-6 xs-fullwidth">
+                                                                <div class="product__price_download">
+                                                                    <div class="item_price v_middle">
+                                                                        <span>@currency((($val->harga_jual - ($val->diskon / 100 * $val->harga_jual)) * $val->jumlah) + $val->ongkir)</span>
+                                                                    </div>
+                                                                    <div class="item_action v_middle">
+                                                                        <a href="{{ URL::to('pesanan/detail/'.$val->id_transaksi_detail) }}" class="btn btn--md btn--round">Detail</a>
+                                                                        <a href="#" class="btn btn--md btn--round btn--white rating--btn not--rated" data-toggle="modal" data-target="#myModal">
+                                                                            <p class="rate_it">Beri Rating</p>
+                                                                            <div class="rating product--rating">
+                                                                                <ul>
+                                                                                    <li>
+                                                                                        <span class="fa fa-star-o"></span>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <span class="fa fa-star-o"></span>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <span class="fa fa-star-o"></span>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <span class="fa fa-star-o"></span>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <span class="fa fa-star-o"></span>
+                                                                                    </li>
+                                                                                </ul>
+                                                                            </div>
+                                                                        </a>
+                                                                        <!-- end /.rating_btn -->
+                                                                    </div>
+                                                                    <!-- end /.item_action -->
+                                                                </div>
+                                                                <!-- end /.product__price_download -->
+                                                            </div>
+                                                            <!-- end /.col-md-4 -->
+                                                        </div>
+                                                    </div>
+                                                    <!-- end /.single_product -->
+                                                </div>
+                                            @endforeach
+                                        @endforeach
+                                    </div>
+                                    <!-- end /.row -->
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="fade show tab-pane product-tab" id="pending">
                             <div class="tab-content-wrapper">
                                 <div class="product_archive table-responsive">
                                     <div class="title_area">
