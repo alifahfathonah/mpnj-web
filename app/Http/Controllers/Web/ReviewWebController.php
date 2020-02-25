06cf34c4652658d39cc21718efe47938df130e32
Masbro <s2gotosingapore@gmail.com>
2020-02-25 22:00:03 +0700

    Add Controller Review

---
+++ app/Http/Controllers/Web/ReviewWebController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReviewWebController extends Controller
+{
+    public function postReview($id_produk){
+        
+    }
+}
