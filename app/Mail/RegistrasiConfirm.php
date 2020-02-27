6ca0ddb05f7d3106af4af01ce1dbd079dc91f3ad
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-28 05:42:55 +0700

    Mail function registrasi confirm

---
+++ app/Mail/RegistrasiConfirm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegistrasiConfirm extends Mailable
+{
+    use Queueable, SerializesModels;
+    protected $id;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('blogsayailham@gmail.com')
+                    ->view('email/registrasi_confirm')
+                    ->with([
+                        'id' => $this->id,
+                    ]);
+    }
+}
