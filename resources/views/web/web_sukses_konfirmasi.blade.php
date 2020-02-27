0deeeb16a585e23b0671d2671bcd7811fb339019
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-28 05:41:18 +0700

    membuat halaman sukses verifikasi akun

---
+++ resources/views/web/web_sukses_konfirmasi.blade.php
@@ -0,0 +1,48 @@
+@extends('web.web_master')
+
+@section('web_konten')
+    <!--================================
+        START BREADCRUMB AREA
+    =================================-->
+    <section class="breadcrumb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="breadcrumb">
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">Verifikasi Akun</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <h1 class="page-title">Sukses</h1>
+                </div>
+                <!-- end /.col-md-12 -->
+            </div>
+            <!-- end /.row -->
+        </div>
+        <!-- end /.container -->
+    </section>
+    <!--================================
+            END BREADCRUMB AREA
+        =================================-->
+    <section class="dashboard-area">
+        <div class="dashboard_contents">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-3">
+                        <div class="cardify signup_form">
+                            <div class="login--header">
+                                <h3>Konfirmasi Akun Sukses</h3>
+                                <p>Terima kasih anda telah melakukan konfirmasi akun, akun anda sekarang telah aktif. Silahkan menikmati kenyamanan berbelanja di situs kami.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
