aba22f55902958a09aa88edf703d3f00dab0af21
Masbro <s2gotosingapore@gmail.com>
2020-02-28 07:06:05 +0700

    ADD Upload Function API & Delete Username

--- app/Http/Controllers/Api/ApiRegisterKonsumenController.php
+++ app/Http/Controllers/Api/ApiRegisterKonsumenController.php
@@ -36,9 +36,7 @@ class ApiRegisterKonsumenController extends Controller
     public function update(Request $request, $kosumenId)
     {
         $konsumen = Konsumen::find($kosumenId);
-
         $konsumen->nama_lengkap = $request->nama_lengkap;
-        $konsumen->username = $request->username;
         $konsumen->nomor_hp = $request->nomor_hp;
         $konsumen->email = $request->email;
         $konsumen->status = $request->status;
@@ -46,7 +44,6 @@ class ApiRegisterKonsumenController extends Controller
         if ($konsumen->save()) {
             $res['pesan'] = "Sukses!";
             $res['data'] = [$konsumen];
-
             return response()->json($res);
         } else {
             $res2['pesan'] = "Gagal!";
@@ -54,5 +51,32 @@ class ApiRegisterKonsumenController extends Controller
 
             return response()->json($res2);
         }
+    }
+       public function upload(Request $request)
+        {
+            $user = Konsumen::where('id_konsumen', $request->id_konsumen)->first();
+            if ($user) {
+                    File::delete('assets/foto_profil_konsumen/' .$user->foto_profil);
+                    $simpan = Konsumen::find($user->id_konsumen);
+                    $file = $request->file('file');
+                    $name = $this->acakhuruf(15) . '.' . $file->getClientOriginalExtension();
+                    $file->move('assets/foto_profil_konsumen', $name);
+                    $simpan->foto_profil = $name;
+                    $simpan->save();
+                    $res['pesan'] = "Sukses!";
+                    $res['data'] = [$simpan];
+                    return response()->json($res);
+            } else {
+                  $res2['pesan'] = "Gagal!";
+            $res2['data'] = [];
+
+            return response()->json($res2);
+            }
+        }
+          public static function acakhuruf($length)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
     }
 }
