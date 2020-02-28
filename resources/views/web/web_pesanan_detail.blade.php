d19a1b390a0fd1fc21fceac6ca0f94d88af507e0
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-28 22:57:39 +0700

    membuat form review produk di halaman invoice

--- resources/views/web/web_pesanan_detail.blade.php
+++ resources/views/web/web_pesanan_detail.blade.php
@@ -161,4 +161,108 @@
             </div>
         </div>
     </section>
+
+    @php $edited = false; @endphp
+
+    @if($detail->transaksi_detail->status_order == 'sukses')
+        <section class="section--padding2 bgcolor" style="margin: 0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="shortcode_modules">
+                            <div class="modules__title">
+                                <h3>Review</h3>
+                            </div>
+                            <div class="modules__content">
+                                @if($review != '')
+                                    <div class="thread thread_review thread_review2" id="reviewBox">
+                                        <ul class="media-list thread-list">
+                                            <li class="single-thread">
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <a href="#">
+                                                            <img class="media-object" src="{{ asset('assets/foto_review/'.$review->foto_review) }}" alt="Commentator Avatar">
+                                                        </a>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <div class="clearfix">
+                                                            <div class="pull-left">
+                                                                <div class="media-heading">
+                                                                    <a href="{{ URL::to('profile') }}">
+                                                                        <h4>{{ $review->konsumen->username }}</h4>
+                                                                    </a>
+{{--                                                                    <a href="#" class="rev_item">Mini - Responsive Bootstrap Dashboard</a>--}}
+                                                                </div>
+                                                                <div class="rating product--rating">
+                                                                    <ul>
+                                                                        @for($i = 1; $i <= $review->bintang; $i++)
+                                                                            <li>
+                                                                                <span class="fa fa-star"></span>
+                                                                            </li>
+                                                                        @endfor
+                                                                    </ul>
+                                                                </div>
+{{--                                                                <span class="review_tag" id="editReview" style="cursor: pointer">Edit</span>--}}
+                                                            </div>
+
+                                                            <div class="pull-right rev_time">{{ $review->updated_at->format('d M Y') }}</div>
+                                                        </div>
+                                                        <p>
+                                                            {{ $review->review }}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                @else
+                                    <form method="post" action="{{ URL::to('review/produk') }}" enctype="multipart/form-data">
+                                        @csrf
+                                        <ul>
+                                            <li>
+                                                <p>Bintang</p>
+                                                <div class="right_content btn btn--round btn--white btn--md">
+                                                    <select name="bintang" class="give_rating">
+                                                        <option value="1">1</option>
+                                                        <option value="2">2</option>
+                                                        <option value="3">3</option>
+                                                        <option value="4">4</option>
+                                                        <option value="5">5</option>
+                                                    </select>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <input type="hidden" name="id_produk" value="{{ $detail->transaksi_detail->produk->id_produk }}">
+                                        <div class="rating_field">
+                                            <label for="rating_field">Komentar</label>
+                                            <textarea name="review" id="rating_field" class="text_field"
+                                                      placeholder="Beri komentar Barang yang Sesuai. "></textarea>
+                                        </div>
+                                        <div class="rating_field">
+                                            <label for="rating_field">Foto</label>
+                                            <input type="file" name="foto_review" id="foto_review" class="form-control">
+                                            <p class="notice">Terima kasih Sudah Mereview Barang Kami. </p>
+                                        </div>
+                                        <button type="submit" class="btn btn--round btn--default">Kirim</button>
+                                    </form>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end .col-md-6 -->
+                </div>
+                <!-- end .row -->
+            </div>
+        </section>
+    @endif
 @endsection
+
+@push('scripts')
+    <script>
+        $(function () {
+           $("#editReview").on('click', function () {
+               $("#reviewBox div").remove()
+           })
+        });
+    </script>
+@endpush
