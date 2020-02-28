d81557373e9a1f8be3d6bff87596f1d62d57234e
Masbro <s2gotosingapore@gmail.com>
2020-02-29 02:46:43 +0700

    Tambah Fungsi Dasboard Pada Controller

--- app/Http/Controllers/Pelapak/PelapakController.php
+++ app/Http/Controllers/Pelapak/PelapakController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Pelapak;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
+use App\Models\Transaksi_Detail;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class PelapakController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('pelapak/home');
+        $role = Session::get('role');
+        $id = Session::get('id');
+        $konsumen_id = $request->user($role)->$id;
+        
+        $data['transaksi'] = DB::table('transaksi')->distinct()
+                ->join('transaksi_detail', 'transaksi_detail.transaksi_id', '=', 'transaksi.id_transaksi')
+                ->where('transaksi_detail.pelapak_id', $konsumen_id)
+                ->where('transaksi_detail.status_order', 'pending')
+                ->get(['id_transaksi','kode_transaksi', 'waktu_transaksi','total_bayar']);
+
+        return view('pelapak/home', $data);
     }
 }
