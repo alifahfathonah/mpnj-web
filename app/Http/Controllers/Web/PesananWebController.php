ee398d33cb7cbca185ce09a3052605d3509953eb
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-28 22:58:05 +0700

    penambahan query review di pesanan web controller

--- app/Http/Controllers/Web/PesananWebController.php
+++ app/Http/Controllers/Web/PesananWebController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Konsumen;
 use App\Models\Pelapak;
+use App\Models\Review;
 use App\Models\Transaksi;
 use App\Models\Transaksi_Detail;
 use Illuminate\Http\Request;
@@ -26,13 +27,19 @@ class PesananWebController extends Controller
         return view('web/web_pesanan', $data);
     }
 
-    public function detail($id_trx)
+    public function detail(Request $request, $id_trx)
     {
+        $role = Session::get('role');
+        $id = Session::get('id');
+        $konsumen_id = $request->user($role)->$id;
+
         $data['detail'] = Transaksi::with(['transaksi_detail' => function ($query) use ($id_trx) {
                             $query->where('id_transaksi_detail', $id_trx);
                         }])
                         ->first();
+        $data['review'] = Review::where('produk_id', $data['detail']->transaksi_detail->produk_id)->where('konsumen_id', $konsumen_id)->first();
 
         return view('web/web_pesanan_detail', $data);
+//        return $data['review'];
     }
 }
