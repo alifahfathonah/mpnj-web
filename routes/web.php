c5a4ddf35e0c9c2ba3571e75b62391a6b1d96986
Masbro <s2gotosingapore@gmail.com>
2020-02-28 07:03:09 +0700

    Add Review Route

--- routes/web.php
+++ routes/web.php
@@ -93,3 +93,6 @@ Route::get('pelapak/{username}/produk', 'Web\PelapakWebController@produk')->name
 
 //pelapak
 Route::get('jual', 'Pelapak\PelapakController@index');
+
+//review
+Route::post('review/produk', 'Web\ReviewWebController@postReview');
