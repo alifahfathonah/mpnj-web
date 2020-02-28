2674ab8a89934bce7723e102e026256872bd535e
Masbro <s2gotosingapore@gmail.com>
2020-02-28 07:05:20 +0700

    Add Minor Function

--- app/Http/Controllers/Web/ReviewWebController.php
+++ app/Http/Controllers/Web/ReviewWebController.php
@@ -4,21 +4,27 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Models\Review;
+use App\Models\Review;
+use Illuminate\Support\Facades\Session;
 
 class ReviewWebController extends Controller
 {
-    public function postReview($id_produk){
+    public function postReview(Request $request){
         $role = Session::get('role');
         $id = Session::get('id');
         $konsumen_id = $request->user($role)->$id;
 
-        $review = Review::create([
+        $review = [
             'konsumen_id' => $konsumen_id,
-            'produk_id' => $request->produk,
+            'produk_id' => $request->id_produk,
             'review' => $request->review,
             'bintang' => $request->bintang,
-            'foto' => $request->foto
-        ]);
+            // 'foto' => $request->foto
+        ];
+
+        $simpan = Review::create($review);
+        if ($simpan) {
+            return redirect()->back();
+        }
     }
 }
