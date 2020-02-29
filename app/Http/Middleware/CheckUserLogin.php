23fb0b4ec66e1cca93b23e5bc557cffeb296ac77
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-02-29 21:28:58 +0700

    membuat middleware check user login

---
+++ app/Http/Middleware/CheckUserLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\URL;
+
+class CheckUserLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Session::exists('id')) {
+            return $next($request);
+        } else {
+            return redirect(URL::to('login'));
+        }
+    }
+}
