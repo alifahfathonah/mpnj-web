0dda14a0393fe73d1f9fb5dd322764294b276f3b
Masbro <s2gotosingapore@gmail.com>
2020-03-02 08:29:01 +0700

    Penambahan Function Popular Produk (Terlaris)

--- app/Http/Controllers/Web/ProdukWebController.php
+++ app/Http/Controllers/Web/ProdukWebController.php
@@ -43,6 +43,14 @@ class ProdukWebController extends Controller
         return view('web/web_produk', $data);
     }
 
+    public function popular(Request $request)
+    {
+        $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->orderBy('terjual', 'DESC')->paginate(9);
+
+        $data['kategori'] = Kategori_Produk::Select('id_kategori_produk', 'nama_kategori')->get();
+        return view('web/web_produk', $data);
+    }
+
     public function produkId($id)
     {
         $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->where('id_produk', $id)->first();
