be96972873af81254edba20e542dbf2a0ee567d6
Masbro <s2gotosingapore@gmail.com>
2020-03-02 08:29:50 +0700

    Tambah Contoh Example Query (Terlaris)

--- app/Http/Controllers/Web/ProdukWebController.php
+++ app/Http/Controllers/Web/ProdukWebController.php
@@ -13,6 +13,7 @@ class ProdukWebController extends Controller
     public function index(Request $request)
     {
         $nama_produk = $request->query('cari');
+
         if ($nama_produk != '') {
             $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->where('nama_produk', 'like', '%'.$nama_produk.'%')->get();
         } else {
@@ -28,12 +29,14 @@ class ProdukWebController extends Controller
         $kategori = $request->query('kategori');
         $nama_produk = $request->query('cari');
         $order = $request->query('order');
+
         if ($kategori != '' OR $order != '') {
             $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->when($kategori != '', function ($query) use ($kategori) {
                 $query->whereHas('kategori', function ($query) use ($kategori) {
                     $query->where('nama_kategori', $kategori != '' ? $kategori : '');
                 });
             })->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->paginate(9);
+            // })->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->orderBy('terjual', $order == 'laris' , 'DESC')->paginate(9);
         } else if ($nama_produk != '') {
             $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->where('nama_produk', 'like', '%'.$nama_produk.'%')->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->paginate(9);
         } else {
