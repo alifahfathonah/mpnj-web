bd9f2b91d936c4997493ba22e24120e83fec3a9f
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-04 01:14:29 +0700

    beri form validator register

--- app/Http/Controllers/Web/KonsumenWebController.php
+++ app/Http/Controllers/Web/KonsumenWebController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class KonsumenWebController extends Controller
 {
@@ -20,22 +21,38 @@ class KonsumenWebController extends Controller
 
     public function simpan(Request $request)
     {
-        $simpan = Konsumen::create([
-            'nama_lengkap' => $request->nama_lengkap,
-            'username' => $request->username,
-            'password' => Hash::make($request->password),
-            'nomor_hp' => $request->nomor_hp,
-            'email' => $request->email
+        $validator = Validator::make($request->except('token'), [
+            'nama_lengkap' => 'required',
+            'username' => 'required|unique:konsumen',
+            'password' => 'required',
+            'nomor_hp' => 'required|min:12|numeric|unique:konsumen',
+            'email' => 'required|email',
         ]);
 
-        if ($simpan) {
-            $credential = $request->only('username','password');
-            if (Auth::guard('konsumen')->attempt($credential)) {
-                Mail::to($request->email)->send(new RegistrasiConfirm($simpan->id_konsumen));
-                $request->session()->put('role', 'konsumen');
-                $request->session()->put('id', 'id_konsumen');
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->with('registerError', 'Register Error. Pastikan data anda sudah benar')
+                ->withInput()
+                ->withErrors($validator);
+        } else {
+            $simpan = Konsumen::create([
+                'nama_lengkap' => $request->nama_lengkap,
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+                'nomor_hp' => $request->nomor_hp,
+                'email' => $request->email
+            ]);
+
+            if ($simpan) {
+                $credential = $request->only('username','password');
+                if (Auth::guard('konsumen')->attempt($credential)) {
+                    Mail::to($request->email)->send(new RegistrasiConfirm($simpan->id_konsumen));
+                    $request->session()->put('role', 'konsumen');
+                    $request->session()->put('id', 'id_konsumen');
+                }
+                return redirect('/');
             }
-            return redirect('/');
         }
     }
 
