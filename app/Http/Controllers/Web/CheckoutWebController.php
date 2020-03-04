cd5f97cc40447296ca880222f8b4e9ba7ab71e68
LuthfiRobit <luthfirobit@gmail.com>
2020-03-05 02:38:48 +0700

    menambah middleware di CheckoutWebController

--- app/Http/Controllers/Web/CheckoutWebController.php
+++ app/Http/Controllers/Web/CheckoutWebController.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Session;
 
 class CheckoutWebController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('checkUserLogin');
+    }
+
     public function index(Request $request)
     {
         $role = Session::get('role');
@@ -31,49 +37,49 @@ class CheckoutWebController extends Controller
         //                 ->get()
         //                 ->groupBy('keranjang.konsumen_id');
         $data['order'] = Keranjang::with(['produk', 'pembeli', 'pembeli.alamat_fix', 'pembeli.daftar_alamat'])
-                        ->where('pembeli_id', $konsumen_id)
-                        ->where('pembeli_type', $role == 'konsumen' ? Konsumen::class : Pelapak::class)
-                        ->where('status', 'Y')
-                        ->get()
-                        ->groupBy('produk.pelapak.nama_toko');
-//        $data['total'] = Keranjang::where('pembeli_id', $konsumen_id)
-//                        ->where('status', 'Y')
-//                        ->sum(DB::raw('jumlah * harga_jual'));
+            ->where('pembeli_id', $konsumen_id)
+            ->where('pembeli_type', $role == 'konsumen' ? Konsumen::class : Pelapak::class)
+            ->where('status', 'Y')
+            ->get()
+            ->groupBy('produk.pelapak.nama_toko');
+        //        $data['total'] = Keranjang::where('pembeli_id', $konsumen_id)
+        //                        ->where('status', 'Y')
+        //                        ->sum(DB::raw('jumlah * harga_jual'));
         $data['berat'] = DB::table("keranjang")
-                        ->select(DB::raw("SUM(produk.berat * keranjang.jumlah) as total_berat"))
-                        ->leftjoin("produk","keranjang.produk_id","=","produk.id_produk")->groupBy('produk.pelapak_id')
-                        ->get();
+            ->select(DB::raw("SUM(produk.berat * keranjang.jumlah) as total_berat"))
+            ->leftjoin("produk", "keranjang.produk_id", "=", "produk.id_produk")->groupBy('produk.pelapak_id')
+            ->get();
 
         return view('web/web_checkout', $data);
     }
-    
+
     public function simpanTransaksi(Request $request)
     {
         $role = Session::get('role');
         $id = Session::get('id');
         $konsumen_id = $request->user($role)->$id;
 
-    	$simpanTrx = Transaksi::create([
-    		'pembeli_id' => $konsumen_id,
+        $simpanTrx = Transaksi::create([
+            'pembeli_id' => $konsumen_id,
             'pembeli_type' => $role == 'konsumen' ? 'App\Models\Konsumen' : 'App\Models\Pelapak',
-		    'kode_transaksi' => time(),
-		    'waktu_transaksi' => date('Y-m-d H:i:s'),
-		    'total_bayar' => $request->totalBayar
-	    ]);
-    	if ($simpanTrx) {
-    		foreach ($request->trxDetail as $detail) {
-    			//buat trigger ketika data masuk ke transaksi detail untuk mengurangi stok produk dan memperbarui field terjual
-    			$detail['transaksi_id'] = $simpanTrx->id_transaksi;
-    			Transaksi_Detail::create($detail);
-		    }
+            'kode_transaksi' => time(),
+            'waktu_transaksi' => date('Y-m-d H:i:s'),
+            'total_bayar' => $request->totalBayar
+        ]);
+        if ($simpanTrx) {
+            foreach ($request->trxDetail as $detail) {
+                //buat trigger ketika data masuk ke transaksi detail untuk mengurangi stok produk dan memperbarui field terjual
+                $detail['transaksi_id'] = $simpanTrx->id_transaksi;
+                Transaksi_Detail::create($detail);
+            }
             Keranjang::whereIn('id_keranjang', $request->idKeranjang)->delete();
-		    return response()->json($simpanTrx,200);
-	    }
+            return response()->json($simpanTrx, 200);
+        }
     }
-    
+
     public function sukses($kodeTrx)
     {
-    	$data['order_sukses'] = Transaksi::where('kode_transaksi', $kodeTrx)->first();
-    	return view('web/web_checkout_sukses', $data);
+        $data['order_sukses'] = Transaksi::where('kode_transaksi', $kodeTrx)->first();
+        return view('web/web_checkout_sukses', $data);
     }
 }
