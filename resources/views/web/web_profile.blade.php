72e9fbf33aa4866bbf21996d4d1b38a9c9619c2b
Fucek17 <do.crazy192@gmail.com>
2020-03-06 02:24:19 +0700

    Update Web Profile
    Signed-off-by: Fucek17 <do.crazy192@gmail.com>

--- resources/views/web/web_profile.blade.php
+++ resources/views/web/web_profile.blade.php
@@ -15,18 +15,12 @@
 
 	<aside class="col-md-3">
 		<!--   SIDEBAR   -->
-		<ul class="list-group">
-			@if(Route::currentRouteName() == 'profile')
-			@include('web.profile.profile')
-			@elseif(Route::currentRouteName() == 'rekening')
-			@include('web.profile.rekening')
-			@elseif(Route::currentRouteName() == 'alamat')
-			@include('web.profile.alamat')
-		   @endif
-			{{-- <a class="list-group-item active" href="#"> My order history </a> --}}
-			
-		</ul>
-		<br>
+		
+		<div class="list-group">
+			<a href="{{ URL::to('profile') }}" class="{{ Route::currentRouteName() == 'profile' ? 'active' : '' }} list-group-item list-group-item-action">Profil</a>
+			<a href="{{ URL::to('profile/alamat') }}" class="{{ Route::currentRouteName() == 'alamat' ? 'active' : '' }} list-group-item list-group-item-action">Alamat</a>
+                                  	
+		  </div>
 
 	<a class="btn btn-light btn-block" href="{{ route('keluar') }}"> <i class="fa fa-power-off"></i> <span class="text">Keluar</span> </a> 
 		<!--   SIDEBAR .//END   -->
@@ -40,11 +34,18 @@
 		<div class="card-body">
 			<div class="row"> 
 				<div class="col-md-8">
-					<h6 class="text-muted">Profile</h6>
+					{{-- <h6 class="text-muted">Profile</h6>
 					<p>{{ Auth::guard(Session::get('role'))->user()->nama_lengkap }} <br>  
 					No Telp : {{ Auth::guard(Session::get('role'))->user()->nomor_hp }}<br>
 			    	Alamat : {{ Auth::guard(Session::get('role'))->user()->alamat_utama }}
-			 		</p>
+					 </p> --}}
+					 @if(Route::currentRouteName() == 'profile')
+					 @include('web.profile.profile')
+					 @elseif(Route::currentRouteName() == 'rekening')
+					 @include('web.profile.rekening')
+					 @elseif(Route::currentRouteName() == 'alamat')
+					 @include('web.profile.alamat')
+					@endif
 				</div>
 				<div class="col-md-4">
 					<h6 class="text-muted">Payment</h6>
@@ -60,24 +61,16 @@
 			</div> <!-- row.// -->
 		</div> <!-- card-body .// -->
 		<div class="table-responsive">
-		<table class="table table-hover">
+		{{-- <table class="table table-hover">
 			<tbody>
 			<tr>
 				<td>
-					<img src="{{ asset('assets/foto_profil_konsumen/'.Auth::guard(Session::get('role'))->user()->foto_profil) }}" alt="Presenting the broken author avatar :D">
-				</td>
-				<td> 
-				
-							<a href="{{ URL::to('profile') }}" class="{{ Route::currentRouteName() == 'profile' ? 'active' : '' }}">Profile</a>
-					<br>
-					
-							<a href="{{ URL::to('profile/alamat') }}" class="{{ Route::currentRouteName() == 'alamat' ? 'active' : '' }}">Alamat</a></li>
-					
+					<img width="200px" src="{{ asset('assets/foto_profil_konsumen/'.Auth::guard(Session::get('role'))->user()->foto_profil) }}" alt="Presenting the broken author avatar :D">
 				</td>
 				<td> Seller <br> ABC shop </td>
 				<td> <a href="#" class="btn btn-outline-primary">Track order</a> <a href="#" class="btn btn-light"> Details </a> </td>
 			</tr>
-		</tbody></table>
+		</tbody></table> --}}
 		</div> <!-- table-responsive .end// -->
 		</article> <!-- order-group.// --> 
 	</main>
