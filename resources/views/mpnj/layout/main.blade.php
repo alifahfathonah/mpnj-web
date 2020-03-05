f5203c517a542bed2eddabb93dc9f82be88b731e
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-05 23:24:17 +0700

    menambah crsf token di meta tag

--- resources/views/mpnj/layout/main.blade.php
+++ resources/views/mpnj/layout/main.blade.php
@@ -5,6 +5,8 @@
 <meta http-equiv="pragma" content="no-cache" />
 <meta http-equiv="cache-control" content="max-age=604800" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link rel="shortcut icon" href="{{ url('assets/mpnj/images/nj.png') }}" type="image/x-icon">
 <head>
     <title>@yield('title')</title>
