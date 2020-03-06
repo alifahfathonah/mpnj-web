d7c15ff4c60f36808ce4155a6da9d09a2b09636f
Dzun <nurroinz@gmail.com>
2020-03-06 20:12:03 +0700

    styling pada profile pelapak

--- public/js/app.js
+++ public/js/app.js
@@ -49443,6 +49443,17 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./public/assets/mpnj/sass/ui.scss":
+/*!*****************************************!*\
+  !*** ./public/assets/mpnj/sass/ui.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleNotFoundError: Module not found: Error: Can't resolve '../images/banners/bg-cpu.jpg' in 'C:\\Users\\nurro\\Documents\\mpnj-web\\public\\assets\\mpnj\\sass'\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\webpack\\lib\\Compilation.js:925:10\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\webpack\\lib\\NormalModuleFactory.js:401:22\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\webpack\\lib\\NormalModuleFactory.js:130:21\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\webpack\\lib\\NormalModuleFactory.js:224:22\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\neo-async\\async.js:2830:7\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\neo-async\\async.js:6877:13\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\webpack\\lib\\NormalModuleFactory.js:214:25\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:213:14\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:13:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\UnsafeCachePlugin.js:44:7\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:13:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:25:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:67:43\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:14:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:25:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:67:43\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:14:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:13:1)\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\DirectoryExistsPlugin.js:27:15\n    at C:\\Users\\nurro\\Documents\\mpnj-web\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -49595,26 +49606,15 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*********************************************************************!*\
+  !*** multi ./resources/js/app.js ./public/assets/mpnj/sass/ui.scss ***!
+  \*********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\myproject\laravel\mpnj-web\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\myproject\laravel\mpnj-web\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\nurro\Documents\mpnj-web\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\nurro\Documents\mpnj-web\public\assets\mpnj\sass\ui.scss */"./public/assets/mpnj/sass/ui.scss");
 
 
 /***/ })
