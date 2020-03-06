0ed7c8cd1396eabe8b8d2d1199179a2a21cd3a69
Gulpi <gulpi.qorik@gmail.com>
2020-03-06 12:04:30 +0700

    Ganti Warna dan Tampilan Header Versi 1 + Ubah detail produk
    Mungkin akan ada perubahan lagi nanti

--- resources/views/mpnj/layout/header.blade.php
+++ resources/views/mpnj/layout/header.blade.php
@@ -1,5 +1,5 @@
 <header class="section-header">
-    <nav class="navbar p-md-0 navbar-expand-lg navbar-light border-bottom">
+    <nav class="navbar p-md-0 navbar-expand-lg navbar-dark shadow-sm p-3 mb-2">
         <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop3" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,7 @@
             </div> <!-- navbar-collapse .// -->
         </div> <!-- container //  -->
     </nav>
-    <section class="header-main border-bottom">
+    <section class="header-main mb-3">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-3 col-4">
@@ -79,7 +79,7 @@
             </div> <!-- row.// -->
         </div> <!-- container.// -->
     </section> <!-- header-main .// -->
-    <nav class="navbar navbar-main navbar-expand-lg border-bottom">
+    <nav class="navbar navbar-main navbar-expand-lg shadow bg-light">
         <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav3" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
