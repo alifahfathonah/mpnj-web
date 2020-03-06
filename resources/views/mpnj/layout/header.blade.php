b64eeecc30e63bb8663c077b5013cb1a9f5e88a6
Gulpi <gulpi.qorik@gmail.com>
2020-03-06 15:25:33 +0700

    Rampingkan header

--- resources/views/mpnj/layout/header.blade.php
+++ resources/views/mpnj/layout/header.blade.php
@@ -1,5 +1,5 @@
-<header class="section-header">
-    <nav class="navbar p-md-0 navbar-expand-lg navbar-dark shadow-sm p-3 mb-2">
+<header class="section-header sticky-top">
+    <nav class="navbar p-md-0 navbar-expand-sm navbar-dark shadow-sm">
         <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop3" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -27,6 +27,23 @@
                     </li> -->
                 </ul>
                 <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link pl-0" href="{{ URL::to('/') }}"> <strong>Home</strong></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('/produk') }}">Semua Produk</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
+                            <div class="dropdown-menu">
+                                @foreach ($kategori as $k)
+                                <a class="dropdown-item" href="{{ URL::to('produk?kategori='.strtolower($k->nama_kategori)) }}">{{ $k->nama_kategori }}</a>
+                                @endforeach
+                            </div>
+                        </li>
+                    </ul>
+                <!--
+                    <ul class="navbar-nav">
                     <li class="nav-item"><a href="#" class="nav-link"> My Account </a></li>
                     <li class="nav-item"><a href="#" class="nav-link"> Wishlist </a></li>
                     <li class="nav-item"><a href="#" class="nav-link"> Checkout </a></li>
@@ -34,7 +51,7 @@
             </div> <!-- navbar-collapse .// -->
         </div> <!-- container //  -->
     </nav>
-    <section class="header-main mb-3">
+    <section class="header-main pt-0 pb-0 shadow">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-3 col-4">
@@ -78,31 +95,33 @@
                 </div> <!-- col.// -->
             </div> <!-- row.// -->
         </div> <!-- container.// -->
-    </section> <!-- header-main .// -->
-    <nav class="navbar navbar-main navbar-expand-lg shadow bg-light">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav3" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="main_nav3">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link pl-0" href="{{ URL::to('/') }}"> <strong>Home</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/produk') }}">Semua Produk</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
-                        <div class="dropdown-menu">
-                            @foreach ($kategori as $k)
-                            <a class="dropdown-item" href="{{ URL::to('produk?kategori='.strtolower($k->nama_kategori)) }}">{{ $k->nama_kategori }}</a>
-                            @endforeach
-                        </div>
-                    </li>
-                </ul>
-            </div> <!-- collapse .// -->
-        </div> <!-- container .// -->
+        <!--
+        <nav class="navbar navbar-expand-sm p-md-0 bg-light mb-0 pb-0">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav3" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="main_nav3">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link pl-0" href="{{ URL::to('/') }}"> <strong>Home</strong></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('/produk') }}">Semua Produk</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
+                            <div class="dropdown-menu">
+                                @foreach ($kategori as $k)
+                                <a class="dropdown-item" href="{{ URL::to('produk?kategori='.strtolower($k->nama_kategori)) }}">{{ $k->nama_kategori }}</a>
+                                @endforeach
+                            </div>
+                        </li>
+                    </ul>
+                </div> <!-- collapse .// -->
+            </div> <!-- container .// -->
     </nav> <!-- navbar main end.// -->
+    </section> <!-- header-main .// -->
+    
     <div class="corner-ribbon bottom-right sticky blue shadow">COMMIT UNUJA</div>
 </header>
