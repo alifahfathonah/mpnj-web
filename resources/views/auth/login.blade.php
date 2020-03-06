d94561e6697d8803e384de420c5bc30620adee83
Fucek17 <do.crazy192@gmail.com>
2020-03-06 13:44:58 +0700

    Update Tampilan Login Dengan Logo MPNJ
    Signed-off-by: Fucek17 <do.crazy192@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,52 +6,13 @@
 @section('content')
 
 
-{{-- 
-<div class="container">
-    <div class="row my-3 justify-content-center">
-        <div class="col-md-7">
-            
-<div class="card card-lg">
-    <div class="card-header">
-        <h5 class="card-title"> Selamat Datang, Silahkan Login</h5>
-    </div>
-    <div class="card-body">
-        @if(session('loginError'))
-        <div class="alert alert-danger" role="alert">
-            <span class="alert_icon lnr lnr-warning"></span>
-            {{ session('loginError') }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span class="lnr lnr-cross" aria-hidden="true"></span>
-            </button>
-        </div>
-    @endif
-        
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-        <div class="form-group">
-           <label for="username">Username</label>
-           <input id="username" name="username" class="form-control form-control-alternative" placeholder="Masukkan Username" type="text">
-        </div> <!-- form-group// -->
-        <div class="form-group">
-          <label for="pass">Password</label>
-          <input id="pass" name="password" class="form-control  form-control-alternative" placeholder="Masukkan Password" type="password">
-        </div>
-       
-        <a href="{{ URL::to('register') }}" class="text-center text-info">Belum punya akun,register</a>
-        <a class="float-right text-info float-right" href="{{ URL::to('password/reset') }}">Lupa Password</a>
-        <br>
-        <br>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  </div>
-    
-</div>
-</div>
-</div> --}}
+
+@push('scripts')
 
 <link rel="stylesheet" type="text/css" href="{{ url('assets/mpnj/css/main.css ') }} ">
 <script src="{{ url('assets/mpnj/js/main.js')}}"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ url('assets/mpnj/css/material-design-iconic-font.min.css') }}>
 
 <div class="limiter">
     <div class="container-login100">
@@ -70,9 +31,9 @@
         <form action="{{ route('login') }}" method="POST">
             @csrf
         <diV>
-            <span class="login100-form-title p-b-30 my-2 font-weight-bold">Login MPNJ</span>
+            <span class="login100-form-title p-b-30 my-2 text-black">Selamat Datang, Silahkan Login!</span>
             <li class="login100-form-title p-b-20 mt-2"><a href="#">
-                    <img width="100px" src="{{ url('assets/mpnj/images/nj.png') }}"></a></li>
+                    <img width="250px" src="{{ asset('assets/mpnj/images/mpnj.jpg') }}"></a></li>
         </div>
 
         <div class="wrap-input100 validate-input" data-validate="username">
@@ -99,7 +60,7 @@
 
                 <div class="login100-form-bgbtn"></div>
                 <button class="login100-form-btn" type="submit">
-                    LOGIN <i class="fa fa-sign-in" aria-hidden="true"></i>
+                    LOGIN <i class="fa fa-fw fa-sign-in fa-lg"></i>
             </div>
         </div>
 
