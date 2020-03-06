74f48657cb52aa53b58f8f6e1cd5ca4b9894e741
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-06 20:13:50 +0700

    refactor tampilan profile
    dan hapus asset foto_profile

--- resources/views/web/profile/profile.blade.php
+++ resources/views/web/profile/profile.blade.php
@@ -1,37 +1,36 @@
-<div class="row my-1">
-    <div class="col-md-10">
-        <h3 >Informasi Pribadi</h3>
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title mb-4">Profile</h4>
         @php
             $id = Session::get('id');
         @endphp
-        <br>
         <form action="{{ URL::to('profile/ubah/'.Session::get('role').'/'.Auth::guard('konsumen')->user()->$id) }}" method="post" enctype="multipart/form-data">
             @csrf
-
             <div class="form-group">
-                <label for="nama_lengkap">Nama</label>
-                <input type="text" id="nama_lengkap" name="nama_lengkap" class="form-control form-control-alternative" placeholder="nama" value="{{ Auth::guard(Session::get('role'))->user()->nama_lengkap }}">
+                <img src="{{ asset('assets/foto_profil_konsumen/'.Auth::guard(Session::get('role'))->user()->foto_profil) }}" class="img-sm rounded-circle border">
             </div>
-
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" class="form-control form-control-alternative" placeholder="email" value="{{ Auth::guard(Session::get('role'))->user()->email }}">
-            </div>
-
-            <div class="form-group">
-                <label for="no_hp">Nomor HP</label>
-                <input type="text" id="no_hp" name="no_hp" class="form-control form-control-alternative" placeholder="Nomor HP" value="{{ Auth::guard(Session::get('role'))->user()->nomor_hp }}">
-            </div>
-
-            <div class="form-group">
-                <label for="cover_photo" class="upload_btn">
+            <div class="form-row">
+                <div class="col form-group">
+                    <label>Nama Lengkap</label>
+                    <input type="text" id="nama_lengkap" name="nama_lengkap" class="form-control" placeholder="nama" value="{{ Auth::guard(Session::get('role'))->user()->nama_lengkap }}">
+                </div>
+                <div class="col form-group">
+                    <label>Email</label>
+                    <input type="email" id="email" name="email" class="form-control form-control-alternative" placeholder="email" value="{{ Auth::guard(Session::get('role'))->user()->email }}">
+                </div> <!-- form-group end.// -->
+            </div> <!-- form-row.// -->
+            <div class="form-row">
+                <div class="col form-group">
+                    <label for="no_hp">Nomor HP</label>
+                    <input type="text" id="no_hp" name="no_hp" class="form-control form-control-alternative" placeholder="Nomor HP" value="{{ Auth::guard(Session::get('role'))->user()->nomor_hp }}">
+                </div>
+                <div class="col form-group">
                     <label for="foto">Foto</label>
                     <input type="file" id="foto_profil" name="foto_profil" class="form-control">
-                </label>
+                </div>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button class="btn btn-primary btn-block" type="submit">Simpan</button>
         </form>
-
-    </div>
+    </div> <!-- card-body.// -->
 </div>
