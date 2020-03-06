3211fdc850a3b2aff0ce7f6bf26a2e423465f1f5
LuthfiRobit <luthfirobit@gmail.com>
2020-03-07 05:35:03 +0700

    minor add required login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('mpnj.layout.main')
 
 @section('title','Login')
-    
+
 
 @section('content')
 
@@ -9,57 +9,57 @@
 <div class="limiter">
     <div class="container-login100">
         <div class="wrap-login100">
-@if(session('loginError'))
+            @if(session('loginError'))
 
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <span class="alert_icon lnr lnr-warning"></span>
-    {{ session('loginError') }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span class="fas fa-times h6" aria-hidden="true"></span>
-    </button>
-</div>
-@endif
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <span class="alert_icon lnr lnr-warning"></span>
+                {{ session('loginError') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span class="fas fa-times h6" aria-hidden="true"></span>
+                </button>
+            </div>
+            @endif
 
-<!-- ========================= SECTION CONTENT ========================= -->
-<section class="section-conten padding-y" style="min-height:84vh">
+            <!-- ========================= SECTION CONTENT ========================= -->
+            <section class="section-conten padding-y" style="min-height:84vh">
 
-<!-- ============================ COMPONENT LOGIN   ================================= -->
-	<div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
-      <div class="card-body">
-      <h4 class="card-title mb-4">Login</h4>
-      <form action="{{ route('login') }}" method="POST">
-      @csrf
-          <div class="form-group">
-			 <input name="username" class="form-control" placeholder="Username" type="text">
-          </div> <!-- form-group// -->
-          <div class="form-group">
-			<input name="password" class="form-control" placeholder="Password" type="password">
-          </div> <!-- form-group// -->
-          
-          <!-- <div class="form-group">
+                <!-- ============================ COMPONENT LOGIN   ================================= -->
+                <div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
+                    <div class="card-body">
+                        <h4 class="card-title mb-4">Login</h4>
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input name="username" class="form-control" placeholder="Username" type="text" required>
+                            </div> <!-- form-group// -->
+                            <div class="form-group">
+                                <input name="password" class="form-control" placeholder="Password" type="password" required>
+                            </div> <!-- form-group// -->
+
+                            <!-- <div class="form-group">
           	<a href="#" class="float-right">Forgot password?</a> 
             <label class="float-left custom-control custom-checkbox"> <input type="checkbox" class="custom-control-input" checked=""> <div class="custom-control-label"> Remember </div> </label>
           </div> form-group form-check .// -->
-          <div class="form-group">
-              <button type="submit" class="btn btn-primary btn-block"> Login  </button>
-          </div> <!-- form-group// -->
-          <div class="mt-2">
-          <a href="{{ URL::to('register') }}" class="text-center">Belum punya akun,register</a>
-            <a class="float-right float-right" href="{{ URL::to('password/reset') }}">Lupa Password</a>
-          </div>
-      </form>
-      </div> <!-- card-body.// -->
-    </div> <!-- card .// -->
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block"> Login </button>
+                            </div> <!-- form-group// -->
+                            <div class="mt-2">
+                                <a href="{{ URL::to('register') }}" class="text-center">Belum punya akun,register</a>
+                                <a class="float-right float-right" href="{{ URL::to('password/reset') }}">Lupa Password</a>
+                            </div>
+                        </form>
+                    </div> <!-- card-body.// -->
+                </div> <!-- card .// -->
 
-     <!-- <a class="text-center mt-4">Don't have account? <a href="#">Sign up</a></p> -->
-     <br><br>
-<!-- ============================ COMPONENT LOGIN  END.// ================================= -->
+                <!-- <a class="text-center mt-4">Don't have account? <a href="#">Sign up</a></p> -->
+                <br><br>
+                <!-- ============================ COMPONENT LOGIN  END.// ================================= -->
 
 
-</section>
-<!-- ========================= SECTION CONTENT END// ========================= -->
-</div>
-</div>
+            </section>
+            <!-- ========================= SECTION CONTENT END// ========================= -->
+        </div>
+    </div>
 
 </div>
 
