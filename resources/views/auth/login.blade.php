e62d499cadcda398462fe3d706f26bca3de6d200
Fucek17 <do.crazy192@gmail.com>
2020-03-06 12:53:41 +0700

    Update Tampilan Login Baru
    Signed-off-by: Fucek17 <do.crazy192@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,56 +5,8 @@
 
 @section('content')
 
-  {{-- <section class="my-5">
-      <div class="row mt-2 justify-content-center">
-        <h1 class="text-danger">Selamat Datang Di MPNJ, Silahkan Login</h1>
-    </div>
-    <div class="row">
-        <div class="col-md-8">
 
-            @if(session('loginError'))
-            <div class="alert alert-danger" role="alert">
-                <span class="alert_icon lnr lnr-warning"></span>
-                {{ session('loginError') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span class="lnr lnr-cross" aria-hidden="true"></span>
-                </button>
-            </div>
-        @endif
-            
-           <div class="card justify-content-center">
-        <div class="card-body">
-            <form action="{{ route('login') }}" method="POST">
-                @csrf
-            <div class="form-group">
-               <label for="username">Username</label>
-               <input id="username" name="username" class="form-control form-control-alternative" placeholder="Masukkan Username" type="Username">
-            </div> <!-- form-group// -->
-            <div class="form-group">
-              <a class="float-right text-info" href="{{ URL::to('password/reset') }}">Lupa Password</a>
-       
-              <label for="pass">Password</label>
-              <input id="pass" name="password" class="form-control  form-control-alternative" placeholder="Masukkan Password" type="password">
-            </div> <!-- form-group// --> 
-            <div class="form-group"> 
-              
-            </div> <!-- form-group form-check .// -->
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block"> Login  </button>
-            </div> <!-- form-group// -->    
-        </form>
-        <a href="{{ URL::to('register') }}" class="text-center text-info">Belum punya akun,register</a>
-        
-        </div> <!-- card-body.// -->
-      </div> <!-- card .// -->
-    </div>
-</div>
-
-</div>
-</div>
-s
-</section> --}}
-
+{{-- 
 <div class="container">
     <div class="row my-3 justify-content-center">
         <div class="col-md-7">
@@ -96,7 +48,70 @@ s
     
 </div>
 </div>
+</div> --}}
+
+<link rel="stylesheet" type="text/css" href="{{ url('assets/mpnj/css/main.css ') }} ">
+<script src="{{ url('assets/mpnj/js/main.js')}}"></script>
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+@if(session('loginError'))
+
+<div class="alert alert-danger" role="alert">
+    <span class="alert_icon lnr lnr-warning"></span>
+    {{ session('loginError') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span class="lnr lnr-cross" aria-hidden="true"></span>
+    </button>
 </div>
+@endif
+
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+        <diV>
+            <span class="login100-form-title p-b-30 my-2 font-weight-bold">Login MPNJ</span>
+            <li class="login100-form-title p-b-20 mt-2"><a href="#">
+                    <img width="100px" src="{{ url('assets/mpnj/images/nj.png') }}"></a></li>
+        </div>
+
+        <div class="wrap-input100 validate-input" data-validate="username">
+            <input class="input100 font-weight-bold" type="text" name="username">
+
+            <span class="focus-input100" data-placeholder="Masukkan Username"></span>
+        </div>
+
+        <div class="wrap-input100 validate-input" data-validate="Enter password">
+            <span class="btn-show-pass">
+                <i class="zmdi zmdi-eye"></i>
+            </span>
+            <input class="input100 font-weight-bold" type="password" name="password" value="">
+            <span class="focus-input100" data-placeholder="Masukkan Password"></span>
+        </div>
+
+        <div style="margin-top: -20px">
+            <a href="{{ URL::to('register') }}" class="text-center text-info">Belum punya akun,register</a>
+            <a class="float-right text-info float-right" href="{{ URL::to('password/reset') }}">Lupa Password</a>
+        </div>
+
+        <div class="container-login100-form-btn mt-1">
+            <div class="wrap-login100-form-btn">
+
+                <div class="login100-form-bgbtn"></div>
+                <button class="login100-form-btn" type="submit">
+                    LOGIN <i class="fa fa-sign-in" aria-hidden="true"></i>
+            </div>
+        </div>
+
+
+</div>
+</form>
+
+</div>
+</div>
+
+</div>
+
 <!--================================
             END LOGIN AREA
     =================================-->
