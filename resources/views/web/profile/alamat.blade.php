18274e17f1e5bd48e65b026ae797248dbe13390e
Masbro <s2gotosingapore@gmail.com>
2020-03-07 00:52:10 +0700

    Tambah Penampil Alert pada Alamat

--- resources/views/web/profile/alamat.blade.php
+++ resources/views/web/profile/alamat.blade.php
@@ -2,6 +2,14 @@
 
 <div class="row">
     <div class="col-md-12">
+        @if (session('alert'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session('alert') }}</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+        </div>
+        @endif
         <div class="information_module">
             <a class="toggle_title">
                 <h4>Data Alamat</h4>
