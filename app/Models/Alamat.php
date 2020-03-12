a55374cb074fee044fe670026879a3b3cc6eda78
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-12 14:17:01 +0700

    perubahan minor fillable model alamat

--- app/Models/Alamat.php
+++ app/Models/Alamat.php
@@ -8,7 +8,23 @@ class Alamat extends Model
 {
     protected $table = 'alamat';
     protected $primaryKey = 'id_alamat';
-    protected $fillable = ['nama', 'nomor_telepon', 'provinsi_id', 'city_id', 'kecamatan_id', 'nama_kecamatan', 'kode_pos', 'alamat_lengkap', 'user_id', 'user_type', 'nama_provinsi', 'nama_kota', 'alamat_santri'];
+    protected $fillable = [
+        'nama',
+        'nomor_telepon',
+        'provinsi_id',
+        'city_id',
+        'kecamatan_id',
+        'nama_kecamatan',
+        'kode_pos',
+        'alamat_lengkap',
+        'user_id',
+        'user_type',
+        'nama_provinsi',
+        'nama_kota',
+        'alamat_santri',
+        'wilayah',
+        'gang'
+    ];
     public $timestamps = false;
 
     public function user()
