e5187f69da43fcf87791c1bac21737c9ac6e6319
Masbro <s2gotosingapore@gmail.com>
2020-03-14 09:01:55 +0700

    Perubahan minor pada modal

--- app/Models/Alamat.php
+++ app/Models/Alamat.php
@@ -8,23 +8,7 @@ class Alamat extends Model
 {
     protected $table = 'alamat';
     protected $primaryKey = 'id_alamat';
-    protected $fillable = [
-        'nama',
-        'nomor_telepon',
-        'provinsi_id',
-        'city_id',
-        'kecamatan_id',
-        'nama_kecamatan',
-        'kode_pos',
-        'alamat_lengkap',
-        'user_id',
-        'user_type',
-        'nama_provinsi',
-        'nama_kota',
-        'alamat_santri',
-        'wilayah',
-        'gang'
-    ];
+    protected $fillable = ['nama', 'nomor_telepon', 'provinsi_id', 'city_id', 'kecamatan_id', 'nama_kecamatan', 'kode_pos', 'alamat_lengkap', 'user_id', 'user_type', 'nama_provinsi', 'nama_kota', 'alamat_santri','santri','wilayah','kamar'];
     public $timestamps = false;
 
     public function user()
