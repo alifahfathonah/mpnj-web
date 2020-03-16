a6bb64483d7af2e2963c636529dd3257135bc9b3
Hafet17 <do.crazy192@gmail.com>
2020-03-16 11:39:42 +0700

    Menambah Jquery3 untuk paginate di script blade
    Signed-off-by: Hafet17 <do.crazy192@gmail.com>

--- resources/views/mpnj/layout/script.blade.php
+++ resources/views/mpnj/layout/script.blade.php
@@ -1,5 +1,6 @@
 <!-- jQuery -->
 <script src="{{ url('assets/mpnj/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
 <!-- Bootstrap4 files-->
 <script src="{{ url('assets/mpnj/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
