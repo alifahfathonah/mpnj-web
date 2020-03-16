f45a72398134f73a26452d912b4400048807a858
LuthfiRobit <luthfirobit@gmail.com>
2020-03-16 23:18:45 +0700

    done issue ganti url kategori yg ketimpa {{ URL::to('produk?kategori='.$produk->kategori->nama_kategori)}}

--- resources/views/web/web_produk_detail.blade.php
+++ resources/views/web/web_produk_detail.blade.php
@@ -2,14 +2,13 @@
 
 @section('title','Market Place PP Nurul Jadid')
 
-
 @section('content')
 
 <section class="py-3 bg-gray">
     <div class="container">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ URL::to('/') }}">Home</a></li>
-            <li class="breadcrumb-item"><a href="{{ URL::to('kategori/'.$produk->kategori->nama_kategori)}}">{{ $produk->kategori->nama_kategori }}</a></li>
+            <li class="breadcrumb-item"><a href="{{ URL::to('produk?kategori='.$produk->kategori->nama_kategori)}}">{{ $produk->kategori->nama_kategori }}</a></li>
             <li class="breadcrumb-item active" aria-current="page">{{ $produk->nama_produk}}</li>
         </ol>
     </div>
