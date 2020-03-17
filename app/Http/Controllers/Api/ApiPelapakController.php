faf76ca0e4fea7058852508c9d8792e761b53bb9
Masbro <s2gotosingapore@gmail.com>
2020-03-17 20:06:42 +0700

    Import use File

--- app/Http/Controllers/Api/ApiPelapakController.php
+++ app/Http/Controllers/Api/ApiPelapakController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PelapakResource;
 use App\Models\Pelapak;
+use File;
 
 use App\Repositories\PelapakRepository;
 
