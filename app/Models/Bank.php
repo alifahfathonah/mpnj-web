a4f167e86d66b0f109b3235f4531a970b6fd422f
LuthfiRobit <luthfirobit@gmail.com>
2020-03-19 09:18:33 +0700

    membuat model bank

---
+++ app/Models/Bank.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bank extends Model
+{
+    protected $table = 'bank';
+    protected $primaryKey = 'id_bank';
+    protected $fillable = ['nama_bank'];
+    public $timestamps = false;
+}
