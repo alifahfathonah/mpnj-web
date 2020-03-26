be950c70758eec33173cced60293799d3050490d
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-27 01:49:25 +0700

    menambah resi pada fillable model Transaksi_Detail

--- app/Models/Transaksi_Detail.php
+++ app/Models/Transaksi_Detail.php
@@ -8,7 +8,21 @@ class Transaksi_Detail extends Model
 {
     protected $table = 'transaksi_detail';
     protected $primaryKey = 'id_transaksi_detail';
-    protected $fillable = ['transaksi_id','produk_id','pelapak_id','jumlah','harga_jual','diskon','kurir','service','ongkir','etd','sub_total','status_order'];
+    protected $fillable = [
+        'transaksi_id',
+        'produk_id',
+        'pelapak_id',
+        'jumlah',
+        'harga_jual',
+        'diskon',
+        'kurir',
+        'service',
+        'ongkir',
+        'etd',
+        'sub_total',
+        'status_order',
+        'resi'
+    ];
 	public $timestamps = false;
 
 	public function produk()
