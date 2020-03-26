3d8ffa94e1a37183522125d67be14c41f9422e46
Masbro <s2gotosingapore@gmail.com>
2020-03-26 17:12:45 +0700

    Pergantian Query (Diskon)

--- app/Http/Controllers/Web/ProdukWebController.php
+++ app/Http/Controllers/Web/ProdukWebController.php
@@ -36,7 +36,7 @@ class ProdukWebController extends Controller
                 $query->whereHas('kategori', function ($query) use ($kategori) {
                     $query->where('nama_kategori', $kategori != '' ? $kategori : '');
                 });
-            })->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->paginate(12);
+            })->orderBy(DB::raw('harga_jual - (diskon / 100 * harga_jual)'), $order == 'high' ? 'DESC' : 'ASC')->paginate(12);
             // })->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->orderBy('terjual', $order == 'laris' , 'DESC')->paginate(12);
         } else if ($nama_produk != '') {
             $data['produk'] = Produk::with(['foto_produk', 'kategori', 'pelapak'])->where('nama_produk', 'like', '%' . $nama_produk . '%')->orderBy('harga_jual', $order == 'high' ? 'DESC' : 'ASC')->paginate(12);
