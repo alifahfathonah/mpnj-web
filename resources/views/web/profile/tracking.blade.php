c980a0620a5a3b2679fe3da35d3cf679ac1dbdcd
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-27 22:01:51 +0700

    halaman tracking (minor)

--- resources/views/web/profile/tracking.blade.php
+++ resources/views/web/profile/tracking.blade.php
@@ -1,89 +1,63 @@
 <output>
 
     <!-- =========================  COMPONENT TRACKING ========================= -->
-    <article class="card">
-        <header class="card-header"> My Orders / Tracking </header>
-        <div class="card-body">
-            <h6>Order ID: <button id="cekTracking" class="btn"></button></h6>
-            <article class="card">
-                <div class="card-body row no-gutters">
-                    <div class="col">
-                        <strong>Delivery Estimate time:</strong> <br>16:40, 12 nov 2018
+    <div class="col-md-12">
+        <article class="card">
+            <header class="card-header"> My Orders / Tracking </header>
+            <div class="card-body">
+                <h6>Order ID: {{ $detail->transaksi->kode_transaksi }}</h6>
+                <article class="card">
+                    <div class="card-body row no-gutters">
+                        <div class="col">
+                            <strong>Estimasi Pengiriman :</strong> <br> {{ $detail->etd }} hari
+                        </div>
+                        <div class="col">
+                            <strong>Kurir :</strong> <br> {{ $detail->kurir }}
+                        </div>
+                        <div class="col">
+                            <strong>Status:</strong> <br> <div id="status">{{ $detail->status_order }}</div>
+                        </div>
+                        <div class="col">
+                            <strong>Resi :</strong> <br> {{ $detail->resi }}
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <strong>Alamat :</strong> <br> <div id="alamat"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col">
-                        <strong>Shipping company:</strong> <br> Fedex, | <i class="fa fa-phone"></i> +123467890
-                    </div>
-                    <div class="col">
-                        <strong>Status:</strong> <br> Picked by the courier
-                    </div>
-                    <div class="col">
-                        <strong>Tracking #:</strong> <br> 98765432123
-                    </div>
+                </article>
+
+                <div class="tracking-wrap" id="trackingHistory">
+
                 </div>
-            </article>
-
-            <div class="tracking-wrap">
-                <div class="step active">
-                    <span class="icon"> <i class="fa fa-check"></i> </span>
-                    <span class="text">Order confirmed</span>
-                </div> <!-- step.// -->
-                <div class="step active">
-                    <span class="icon"> <i class="fa fa-user"></i> </span>
-                    <span class="text"> Picked by courier</span>
-                </div> <!-- step.// -->
-                <div class="step">
-                    <span class="icon"> <i class="fa fa-truck"></i> </span>
-                    <span class="text"> On the way </span>
-                </div> <!-- step.// -->
-                <div class="step">
-                    <span class="icon"> <i class="fa fa-box"></i> </span>
-                    <span class="text">Ready for pickup</span>
-                </div> <!-- step.// -->
-            </div>
 
 
-            <hr>
-            <ul class="row">
-                <li class="col-md-4">
-                    <figure class="itemside  mb-3">
-                        <div class="aside"><img src="bootstrap-ecommerce-html/images/items/1.jpg" class="img-sm border"></div>
-                        <figcaption class="info align-self-center">
-                            <p class="title">Just name of title or <br> some name goes here</p>
-                            <span class="text-muted">$145 </span>
-                        </figcaption>
-                    </figure>
-                </li>
-                <li class="col-md-4">
-                    <figure class="itemside  mb-3">
-                        <div class="aside"><img src="bootstrap-ecommerce-html/images/items/2.jpg" class="img-sm border"></div>
-                        <figcaption class="info align-self-center">
-                            <p class="title">Great demo product title <br> or name goes here</p>
-                            <span class="text-muted">$250 </span>
-                        </figcaption>
-                    </figure>
-                </li>
-                <li class="col-md-4">
-                    <figure class="itemside mb-3">
-                        <div class="aside"><img src="bootstrap-ecommerce-html/images/items/3.jpg" class="img-sm border"></div>
-                        <figcaption class="info align-self-center">
-                            <p class="title">Another demo product title <br> or name goes here</p>
-                            <span class="text-muted">$145 </span>
-                        </figcaption>
-                    </figure>
-                </li>
-            </ul>
+                <hr>
+                <ul class="row">
+                    <li class="col-md-12">
+                        <figure class="itemside  mb-3">
+                            <div class="aside"><img src="{{ asset('assets/foto_produk/'.$detail->produk->foto_produk[0]->foto_produk) }}" class="img-sm border"></div>
+                            <figcaption class="info align-self-center">
+                                <p class="title">{{ $detail->produk->nama_produk }} <br></p>
+                                <span class="text-muted">$145 </span>
+                            </figcaption>
+                        </figure>
+                    </li>
+                </ul>
 
 
-            <p><strong>Note: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p><strong>Note: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-            <a href="#" class="btn btn-light"> <i class="fa fa-chevron-left"></i> Back to orders</a>
-        </div> <!-- card-body.// -->
-    </article>
+                <a href="#" class="btn btn-light"> <i class="fa fa-chevron-left"></i> Kembali ke Pesanan</a>
+            </div> <!-- card-body.// -->
+        </article>
+    </div>
     <!-- =========================  COMPONENT TRACKING END.// ========================= -->
 </output>
 
@@ -95,17 +69,29 @@
                 url: "{{ URL::to('api/gateway/tracking') }}",
                 type: 'POST',
                 data: {
-                  'waybill' : "{{ $resi }}",
-                  'courier' : "{{ $kurir }}"
+                    'waybill' : "{{ $detail->resi }}",
+                    'courier' : "{{ $detail->kurir }}"
                 },
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
                 beforeSend: function(xhr) {
-                  $("#cekTracking").html('WAIT');
+                    $("#cekTracking").html('WAIT');
                 },
                 success: function (response) {
                     $("#cekTracking").html('SUKSES');
+                    const res = response.waybill.rajaongkir.result;
+                    $("#status").html(res.summary.status)
+                    $("#alamat").html(res.details.receiver_name + " " + res.details.receiver_address1 + " " +res.details.receiver_address2 + " " + res.details.receiver_address3 + " " + res.details.receiver_city);
+                    res.manifest.forEach(item => {
+                       $("#trackingHistory").append(
+                           `<div class="step active">
+                                <span class="icon"> <i class="fa fa-check"></i> </span>
+                                <span class="text">${item.manifest_description} - ${item.manifest_date + " : " + item.manifest_time}</span>
+                            </div>`
+                       )
+                    });
+
                     console.log(response);
                 },
                 error: function (error) {
