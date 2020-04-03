77b9c64bb89c06e053e1f152c8c0e7ef319c5ae4
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-03 14:27:35 +0700

    membuat fungsi produk di api keranjang controller

--- app/Http/Controllers/Api/ApiKategoriController.php
+++ app/Http/Controllers/Api/ApiKategoriController.php
@@ -14,4 +14,25 @@ class ApiKategoriController extends Controller
         $kategori = Kategori_Produk::all();
         return response()->json($kategori, 200);
     }
+
+    public function produk($id)
+    {
+        $produk = Kategori_Produk::with('produk')->where('id_kategori_produk', $id)->first();
+        if ($produk != null) {
+            if ($produk->produk->count() > 0) {
+                return response()->json($produk, 200);
+            } else {
+                return response()->json([
+                    'id_kategori_produk' => $produk->id_kategori_produk,
+                    'nama_kategori' => $produk->nama_kategori,
+                    'produk' => null
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => 404,
+                'pesan' => 'data tidak ditemukan'
+            ], 404);
+        }
+    }
 }
