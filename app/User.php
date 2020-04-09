98e459fe88c53111fa1054670488171e21cdadbf
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-09 22:05:50 +0700

    perubahan model user

--- app/User.php
+++ app/User.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+use App\Models\Alamat;
+use App\Models\Keranjang;
+use App\Models\Produk;
+use App\Models\Transaksi;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -9,6 +13,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    public $guard = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +21,23 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nama_lengkap',
+        'username',
+        'password',
+        'nomor_hp',
+        'foto_profil',
+        'email',
+        'status_official',
+        'role',
+        'nama_toko',
+        'alamat_toko',
+        'foto_toko',
+        'rating',
+        'saldo',
+        'status',
+        'alamat_utama',
+        'remember_token',
+        'alamat_toko',
     ];
 
     /**
@@ -28,12 +49,42 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function transaksi()
+    {
+        return $this->hasMany(Transaksi::class, 'user_id', 'id_user');
+    }
+
+    public function keranjang()
+    {
+        return $this->hasMany(Keranjang::class, 'user_id', 'id_user');
+    }
+
+    public function alamat()
+    {
+        return $this->hasMany(Alamat::class, 'user_id', 'id_user');
+    }
+
+    public function alamat_fix()
+    {
+        return $this->hasOne(Alamat::class, 'id_alamat', 'alamat_utama');
+    }
+
+    public function daftar_alamat()
+    {
+        return $this->hasMany(Alamat::class, 'id_alamat', 'alamat_utama');
+    }
+
+    public function produk()
+    {
+        return $this->hasMany(Produk::class, 'user_id', 'id_user')->where('role', '=', 'pelapak');
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+//    protected $casts = [
+//        'email_verified_at' => 'datetime',
+//    ];
 }
