0cc8c49891ebba78b42b905f02765b2e39519a7d
LuthfiRobit <luthfirobit@gmail.com>
2020-04-19 00:21:38 +0700

    nambain link ler bootstrap.min.css biar tampilan lebih smooth

--- resources/views/mpnj/layout/style.blade.php
+++ resources/views/mpnj/layout/style.blade.php
@@ -1,12 +1,12 @@
-
-
-<link href="{{ url('assets/mpnj/css/bootstrap.css') }}" rel="stylesheet" type="text/css"/>
+<!-- Bootstrap 4.3.1 -->
+<link href="{{ url('assets/mpnj/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+//akan digunakan jika disetujui oleh pembimbing
+{{-- <link href="{{ url('assets/mpnj/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" /> --}}
 
 <!-- Font awesome 5 -->
 <link href="{{ url('assets/mpnj/fonts/fontawesome/css/all.min.css') }}" type="text/css" rel="stylesheet">
 
 <!-- custom style -->
-<link href="{{ url('assets/mpnj/css/ui.css') }}" rel="stylesheet" type="text/css"/>
+<link href="{{ url('assets/mpnj/css/ui.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ url('assets/mpnj/css/responsive.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ url('assets/mpnj/css/experimental.css') }}" rel="stylesheet" type="text/css"/>
-
+<link href="{{ url('assets/mpnj/css/experimental.css') }}" rel="stylesheet" type="text/css" />
