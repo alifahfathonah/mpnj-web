4fd1bdf1678d28ba7f26dab12979c2d2dc0b31d6
LuthfiRobit <luthfirobit@gmail.com>
2020-05-03 22:38:59 +0700

    implentasi produk diskon di home

--- resources/views/web/web_home.blade.php
+++ resources/views/web/web_home.blade.php
@@ -104,61 +104,19 @@
                 </div>
             </div> <!-- col.// -->
             <div class="row no-gutters items-wrap">
+                @foreach($produkDiskon as $pD)
                 <div class="col-md col-6">
                     <figure class="card-product-grid card-sm">
                         <a href="#" class="img-wrap">
-                            <img src="{{ url('assets/mpnj/images/items/3.jpg') }}">
+                            <img src="{{asset('assets/foto_produk/'.$pD->foto_produk[0]->foto_produk) }}">
                         </a>
-                        <div class="text-wrap p-3">
-                            <a href="#" class="title">Summer clothes</a>
-                            <span class="badge badge-danger"> -20% </span>
+                        <div class="namaProduk-rapi">
+                            <a href="{{ URL::to('produk/'.$pD->slug) }}" class="title">{{ $pD->nama_produk }}</a>
                         </div>
+                        <span class="badge badge-danger"> -{{$pD->diskon}}% </span>
                     </figure>
                 </div>
-                <div class="col-md col-6">
-                    <figure class="card-product-grid card-sm">
-                        <a href="#" class="img-wrap">
-                            <img src="{{ url('assets/mpnj/images/items/4.jpg') }}">
-                        </a>
-                        <div class="text-wrap p-3">
-                            <a href="#" class="title">Some category</a>
-                            <span class="badge badge-danger"> -5% </span>
-                        </div>
-                    </figure>
-                </div>
-                <div class="col-md col-6">
-                    <figure class="card-product-grid card-sm">
-                        <a href="#" class="img-wrap">
-                            <img src="{{ url('assets/mpnj/images/items/5.jpg') }}">
-                        </a>
-                        <div class="text-wrap p-3">
-                            <a href="#" class="title">Another category</a>
-                            <span class="badge badge-danger"> -20% </span>
-                        </div>
-                    </figure>
-                </div>
-                <div class="col-md col-6">
-                    <figure class="card-product-grid card-sm">
-                        <a href="#" class="img-wrap">
-                            <img src="{{ url('assets/mpnj/images/items/6.jpg') }}">
-                        </a>
-                        <div class="text-wrap p-3">
-                            <a href="#" class="title">Home apparel</a>
-                            <span class="badge badge-danger"> -15% </span>
-                        </div>
-                    </figure>
-                </div>
-                <div class="col-md col-6">
-                    <figure class="card-product-grid card-sm">
-                        <a href="#" class="img-wrap">
-                            <img src="{{ url('assets/mpnj/images/items/7.jpg') }}">
-                        </a>
-                        <div class="text-wrap p-3">
-                            <a href="#" class="title text-truncate">Smart watches</a>
-                            <span class="badge badge-danger"> -10% </span>
-                        </div>
-                    </figure>
-                </div>
+                @endforeach
             </div>
         </div>
 
