02f44192b9ea8c90cc6f4c1cbc4004844a11dbb0
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-05-05 00:15:44 +0700

    hapus menu dan modal konfirmasi

--- resources/views/web/web_profile.blade.php
+++ resources/views/web/web_profile.blade.php
@@ -21,8 +21,6 @@
                            class="{{ Route::currentRouteName() == 'alamat' ? 'active' : '' }} list-group-item list-group-item-action">Alamat</a>
                         <a href="{{ URL::to('pesanan') }}"
                            class="{{ Route::currentRouteName() == 'pesanan' ? 'active' : '' }} list-group-item list-group-item-action">Transaksi</a>
-                        <a href="#" class="list-group-item list-group-item-action" data-target="#modalKonfirmasi"
-                           data-toggle="modal">Konfirmasi</a>
                         <a href="#" class="list-group-item list-group-item-action" data-target="#modalPassword"
                            data-toggle="modal">Ganti Password</a>
                     </div>
@@ -62,32 +60,6 @@
             </div>
         </div>
     </section>
-    <div class="modal fade" id="modalKonfirmasi" role="dialog" aria-labelledby="exampleModalCenterTitle"
-         aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Masukkan Kode Transaksi</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body text-center">
-                    <form action="{{ route('cek') }}">
-                        <div class="form-group mb-0">
-                            <input type="input" name="kodeTransaksi" class="form-control" id="kodeTransaksi"
-                                   placeholder="Kode Transaksi" required>
-                            <label for="exampleInputEmail1" class="mb-0">Salin Kode Transaksi Anda Disini</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Kirim</button>
-                    </form>
-                </div>
-                <div class="modal-footer">
-
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="modal fade" id="modalPassword" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
