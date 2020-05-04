a27689cf251e9049e257ff7fc07c0e8b5ad40e1b
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-05-05 00:14:31 +0700

    hapus routing get cek kode transaksi

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,6 @@ Route::group(['namespace' => 'Web'], function () {
         Route::get('/', 'KonfirmasiWebController@index')->middleware('checkUserLogin');
         Route::get('data', 'KonfirmasiWebController@data')->middleware('checkUserLogin');
         Route::get('data/{id}', 'KonfirmasiWebController@data');
-        Route::get('cek', 'KonfirmasiWebController@cek')->name('cek');
         Route::post('simpan', 'KonfirmasiWebController@simpan');
         Route::get('akun/{id}', 'KonfirmasiWebController@akun');
         Route::get('verified', 'KonfirmasiWebController@verified');
