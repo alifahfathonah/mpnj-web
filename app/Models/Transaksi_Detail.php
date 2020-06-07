b87c0620ebf70230aafb323e50bd412ac9a6e7eb
LuthfiRobit <luthfirobit@gmail.com>
2020-06-07 11:20:31 +0700

    hapus where user.role di user relation

--- app/Models/Transaksi_Detail.php
+++ app/Models/Transaksi_Detail.php
@@ -26,20 +26,20 @@ class Transaksi_Detail extends Model
         'from_city_id',
         'from'
     ];
-	public $timestamps = true;
+    public $timestamps = true;
 
-	public function produk()
+    public function produk()
     {
         return $this->belongsTo(Produk::class, 'produk_id', 'id_produk');
     }
 
     public function transaksi()
     {
-        return $this->belongsTo(Transaksi::class,'transaksi_id', 'id_transaksi');
+        return $this->belongsTo(Transaksi::class, 'transaksi_id', 'id_transaksi');
     }
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id_user')->where('user.role', '=', 'pelapak');
+        return $this->belongsTo(User::class, 'user_id', 'id_user');
     }
 }
