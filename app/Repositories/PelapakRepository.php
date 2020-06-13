32bd17e407fe07adb7d6efe4e9206825e3052d41
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-06-13 13:04:23 +0700

    buat fungsi cari di produk repository

--- app/Repositories/PelapakRepository.php
+++ app/Repositories/PelapakRepository.php
@@ -59,4 +59,46 @@ class PelapakRepository
                 }
             );
     }
+
+    public function cari($nama)
+    {
+        return Produk::orderBy('id_produk')
+            ->with('foto_produk', 'user', 'kategori')
+            ->where('nama_produk', 'like', '%'.$nama.'%')
+            ->get()
+            ->map(
+                function ($produks) {
+                    return [
+                        'id_produk' => $produks->id_produk,
+                        'nama_produk' => $produks->nama_produk,
+                        'kategori' => [
+                            'id_kategori' => $produks->kategori->id_kategori_produk,
+                            'nama_kategori' => $produks->kategori->nama_kategori
+                        ],
+                        'satuan' => $produks->satuan,
+                        'berat' => $produks->berat,
+                        'harga_modal' => $produks->harga_modal,
+                        'harga_jual' => $produks->harga_jual,
+                        'diskon' => $produks->diskon,
+                        'stok' => $produks->stok,
+                        'keterangan' => $produks->keterangan,
+                        'tipe_produk' => $produks->tipe_produk,
+                        'wishlist' => $produks->wishlist,
+                        'terjual' => $produks->terjual,
+                        'foto' => $produks->foto_produk->map(function ($foto) {
+                            return [
+                                'id_foto_poroduk' => $foto->id_foto_produk,
+                                'foto_produk' => $foto->foto_produk
+                            ];
+                        }),
+                        'pelapak' => [
+                            'id_pelapak' => $produks->user->id_pelapak,
+                            'nama_toko' => $produks->user->nama_toko,
+                            'foto_pelapak' => $produks->user->foto_profil,
+                            'alamat' => $produks->user->alamat
+                        ]
+                    ];
+                }
+            );
+    }
 }
