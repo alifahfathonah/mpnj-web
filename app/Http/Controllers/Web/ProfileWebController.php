a71563ba83dbad8a4bb0896a1cd0d1453e759bc7
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-06-14 14:22:13 +0700

    use Facades DB

--- app/Http/Controllers/Web/ProfileWebController.php
+++ app/Http/Controllers/Web/ProfileWebController.php
@@ -11,6 +11,7 @@ use App\User;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Hash;
