22e8074d3d84c95e9f086525db150b2fdadadb52
LuthfiRobit <luthfirobit@gmail.com>
2020-06-24 00:57:15 +0700

    get bank pake repository

--- app/Http/Controllers/Api/ApiBankController.php
+++ app/Http/Controllers/Api/ApiBankController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Bank;
+use App\Repositories\BankRepository;
 
 class ApiBankController extends Controller
 {
+
+    private $bankRepository;
+
+    public function __construct(BankRepository $bankRepository)
+    {
+        $this->bankRepository = $bankRepository;
+    }
+
     public function index()
     {
-        $kategori = Bank::all();
-        return response()->json($kategori, 200);
+        $bank = $this->bankRepository->all();
+        return $bank;
     }
 }
