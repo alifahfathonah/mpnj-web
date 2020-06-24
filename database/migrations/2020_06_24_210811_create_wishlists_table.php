ffe3d82182eabd364aeb8a9f4f42a8ea8c2d808e
LuthfiRobit <luthfirobit@gmail.com>
2020-06-25 00:55:09 +0700

    wishlist migration

---
+++ database/migrations/2020_06_24_210811_create_wishlists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWishlistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->bigIncrements('id_wishlist');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id_user')->on('users');
+            $table->integer('produk_id')->unsigned();
+            $table->foreign('produk_id')->references('id_produk')->on('produk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlists');
+    }
+}
