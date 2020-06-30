b8bb81603bc2d8a6f642817c39058bee4e6a011a
LuthfiRobit <luthfirobit@gmail.com>
2020-06-30 17:03:52 +0700

    repository pelapak find by name

--- app/Repositories/PelapakRepository.php
+++ app/Repositories/PelapakRepository.php
@@ -27,7 +27,7 @@ class PelapakRepository
                         'email' => $pelapaks->email,
                         'rating' => $pelapaks->rating,
                         'saldo' => $pelapaks->saldo,
-                        'status'=> $pelapaks->status,
+                        'status' => $pelapaks->status,
                     ];
                 }
             );
@@ -38,24 +38,43 @@ class PelapakRepository
         return Pelapak::where('id_pelapak', $id_pelapak)
             ->get()
             ->map(
-                function ($pelapaks)
-                {
+                function ($pelapaks) {
                     return [
-                    'id_pelapak' => $pelapaks->id_pelapak,
-                    'username' => $pelapaks->username,
-                    'status_official' => $pelapaks->status_official,
-                    'nama_toko' => $pelapaks->nama_toko,
-                    'alamat_toko' => $pelapaks->alamat_toko,
-                    'provinsi_id' => $pelapaks->provinsi_id,
-                    'city_id' => $pelapaks->city_id,
-                    'alamat' => $pelapaks->alamat,
-                    'kode_pos' => $pelapaks->kode_pos,
-                    'nomor_hp' => $pelapaks->nomor_hp,
-                    'email' => $pelapaks->email,
-                    'rating' => $pelapaks->rating,
-                    'saldo' => $pelapaks->saldo,
-                    'status'=> $pelapaks->status,
-                ];
+                        'id_pelapak' => $pelapaks->id_pelapak,
+                        'username' => $pelapaks->username,
+                        'status_official' => $pelapaks->status_official,
+                        'nama_toko' => $pelapaks->nama_toko,
+                        'alamat_toko' => $pelapaks->alamat_toko,
+                        'provinsi_id' => $pelapaks->provinsi_id,
+                        'city_id' => $pelapaks->city_id,
+                        'alamat' => $pelapaks->alamat,
+                        'kode_pos' => $pelapaks->kode_pos,
+                        'nomor_hp' => $pelapaks->nomor_hp,
+                        'email' => $pelapaks->email,
+                        'rating' => $pelapaks->rating,
+                        'saldo' => $pelapaks->saldo,
+                        'status' => $pelapaks->status,
+                    ];
+                }
+            );
+    }
+
+    public function findByName($username)
+    {
+        return User::where('username', $username)
+            ->get()
+            ->map(
+                function ($pelapak) {
+                    return [
+                        'id_pelapak' => $pelapak->id_user,
+                        'username' => $pelapak->username,
+                        'nama_toko' => $pelapak->nama_toko,
+                        'nomor_hp' => $pelapak->nomor_hp,
+                        'email' => $pelapak->email,
+                        'rating' => $pelapak->rating,
+                        'foto_profil' => $pelapak->foto_profil,
+                        'foto_toko' => $pelapak->foto_toko
+                    ];
                 }
             );
     }
