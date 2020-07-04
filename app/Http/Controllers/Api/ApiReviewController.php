4b0e442ed8bdc12471575ee2b5b6a2d46593ef16
LuthfiRobit <luthfirobit@gmail.com>
2020-07-05 00:01:15 +0700

    fungsi simpan review

--- app/Http/Controllers/Api/ApiReviewController.php
+++ app/Http/Controllers/Api/ApiReviewController.php
@@ -28,4 +28,31 @@ class ApiReviewController extends Controller
             return response()->json($res2);
         };
     }
+
+    public function simpan(Request $request)
+    {
+        $file = $request->file('file');
+        $name = uniqid() . '_foto_review_' . trim($file->getClientOriginalName());
+
+        $review = [
+            'user_id' => $request->id_user,
+            'produk_id' => $request->id_produk,
+            'review' => $request->review,
+            'bintang' => $request->bintang,
+            'foto_review' => $name
+        ];
+
+        $simpanReview = Review::create($review);
+
+        if ($simpanReview) {
+            $file->move('assets/foto_review/', $name);
+            $res['pesan'] = "Sukses Review!";
+            $res['data'] = $simpanReview;
+
+            return response()->json($res, 201);
+        } else {
+            $res2['pesan'] = "Gagal Review!";
+            return response()->json($res2);
+        }
+    }
 }
