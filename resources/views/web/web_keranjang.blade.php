94a828d55560fa32693c05af1a772638eb2afcd1
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-07-08 21:54:16 +0700

    fixing validasi keranjang
    validasi jika user tidak memilih barang lalu tekan tombol lanjut ke checkout

--- resources/views/web/web_keranjang.blade.php
+++ resources/views/web/web_keranjang.blade.php
@@ -4,13 +4,13 @@
 
 
 @section('content')
-<section class="section-content padding-y">
-    <div class="container">
-        <div class="row">
-            <main class="col-md-9">
-                <div class="card table-responsive">
-                    <table class="table table-borderless table-shopping-cart">
-                        <thead class="text-muted">
+    <section class="section-content padding-y">
+        <div class="container">
+            <div class="row">
+                <main class="col-md-9">
+                    <div class="card table-responsive">
+                        <table class="table table-borderless table-shopping-cart">
+                            <thead class="text-muted">
                             <tr class="small text-uppercase">
                                 <th>#</th>
                                 <th scope="col" class="text-dark">Produk</th>
@@ -19,117 +19,141 @@
                                 <th scope="col" class="text-dark" width="120">Sub Harga</th>
                                 <th scope="col" class="text-right" width="200"></th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <?php $n = 1;
                             $total = 0; ?>
                             @if($data_keranjang->count() > 0)
-                            @foreach($data_keranjang as $val)
-                            <tr id="dataCart">
-                                <td colspan="5"><strong>{{ $val['nama_toko'] }}</strong></td>
-                            </tr>
-                            @foreach ($val['item'] as $k)
-                            <tr id="data_keranjang{{ $k['id_keranjang']  }}" class="sum"
-                                data-total="{{ $total += ($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual'])) * $k['jumlah'] }}"
-                                data-subtotal="{{ ($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual'])) * $k['jumlah'] }}"
-                                data-hargajual="{{ $k['harga_jual']  }}" data-diskon="{{ $k['diskon'] }}"
-                                data-stok="{{ $k['stok'] }}" data-jumlah="{{ $k['jumlah'] }}">
-                                <td>
-                                    <label class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" name="check"
-                                            id="check{{ $k['id_keranjang'] }}" value="{{ $k['id_keranjang'] }}"
-                                            checked="true">
-                                        <div class="custom-control-label"></div>
-                                    </label>
-                                </td>
-                                <td>
-                                    <figure class="itemside">
-                                        <div class="aside"><img src="{{ env('FILES_ASSETS').$k['foto'] }}"
-                                                class="img-sm">
-                                        </div>
-                                        <figcaption class="info">
-                                            <a href="{{ URL::to('produk/'.$k['slug']) }}"
-                                                class="title text-dark">{{ $k['nama_produk'] }}</a>
-                                            <p class="text-muted small" style="color: black">Kategori:
-                                                {{ $k['kategori'] }}</p>
-                                        </figcaption>
-                                    </figure>
-                                </td>
-                                <td class="bold" id="harga{{ $n }}">
-                                    @if($k['diskon'] == 0)
-                                    @currency($k['harga_jual'])
-                                    @else
-                                    <strike style="color: red">@currency($k['harga_jual'])</strike> |
-                                    @currency($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual']))
-                                    @endif
-                                </td>
-                                <td>
-                                    <input type="number" name="qty" id="qty{{ $n }}"
-                                        class="form-control form-control-sm"
-                                        value="{{ $k['jumlah'] != 0 ? $k['jumlah'] : 1 }}">
-                                    <small id="stokLebih{{ $n }}" style="color: red;"></small>
-                                </td>
-                                <td id="subHarga{{ $n }}">
-                                    @if($k['diskon'] == 0)
-                                    @currency($k['harga_jual'] * $k['jumlah'])
-                                    @else
-                                    @currency(($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual'])) *
-                                    $k['jumlah'])
-                                    @endif
-                                </td>
-                                <td class="text-right">
-                                    <a href="{{ URL::to('keranjang/hapus/'.$k['id_keranjang']) }}"
-                                        class="btn btn-light"> Hapus</a>
-                                </td>
-                            </tr>
-                            <?php $n++; ?>
-                            @endforeach
-                            @endforeach
+                                @foreach($data_keranjang as $val)
+                                    <tr id="dataCart">
+                                        <td colspan="5"><strong>{{ $val['nama_toko'] }}</strong></td>
+                                    </tr>
+                                    @foreach ($val['item'] as $k)
+                                        <tr id="data_keranjang{{ $k['id_keranjang']  }}" class="sum"
+                                            data-total="{{ $total += ($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual'])) * $k['jumlah'] }}"
+                                            data-subtotal="{{ ($k['harga_jual'] - ($k['diskon'] / 100 * $k['harga_jual'])) * $k['jumlah'] }}"
+                                            data-hargajual="{{ $k['harga_jual']  }}" data-diskon="{{ $k['diskon'] }}"
+                                            data-stok="{{ $k['stok'] }}" data-jumlah="{{ $k['jumlah'] }}">
+                                            <td>
+                                                <label class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" name="check"
+                                                           id="check{{ $k['id_keranjang'] }}"
+                                                           value="{{ $k['id_keranjang'] }}"
+                                                           checked="true">
+                                                    <div class="custom-control-label"></div>
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <figure class="itemside">
+                                                    <div class="aside"><img src="{{ env('FILES_ASSETS').$k['foto'] }}"
+                                                                            class="img-sm">
+                                                    </div>
+                                                    <figcaption class="info">
+                                                        <a href="{{ URL::to('produk/'.$k['slug']) }}"
+                                                           class="title text-dark">{{ $k['nama_produk'] }}</a>
+                                                        <p class="text-muted small" style="color: black">Kategori:
+                                                            {{ $k['kategori'] }}</p>
+                                                    </figcaption>
+                                                </figure>
+                                            </td>
+                                            <td class="bold" id="harga{{ $n }}">
+                                                @if($k['diskon'] == 0)
+                                                    @currency($k['harga_jual'])
+                                                @else
+                                                    <strike style="color: red">@currency($k['harga_jual'])</strike> |
+                                                    @currency($k['harga_jual'] - ($k['diskon'] / 100 *
+                                                    $k['harga_jual']))
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <input type="number" name="qty" id="qty{{ $n }}"
+                                                       class="form-control form-control-sm"
+                                                       value="{{ $k['jumlah'] != 0 ? $k['jumlah'] : 1 }}">
+                                                <small id="stokLebih{{ $n }}" style="color: red;"></small>
+                                            </td>
+                                            <td id="subHarga{{ $n }}">
+                                                @if($k['diskon'] == 0)
+                                                    @currency($k['harga_jual'] * $k['jumlah'])
+                                                @else
+                                                    @currency(($k['harga_jual'] - ($k['diskon'] / 100 *
+                                                    $k['harga_jual'])) *
+                                                    $k['jumlah'])
+                                                @endif
+                                            </td>
+                                            <td class="text-right">
+                                                <a href="{{ URL::to('keranjang/hapus/'.$k['id_keranjang']) }}"
+                                                   class="btn btn-light"> Hapus</a>
+                                            </td>
+                                        </tr>
+                                        <?php $n++; ?>
+                                    @endforeach
+                                @endforeach
                             @else
-                            <tr>
-                                <td colspan="6" style="text-align: center">Tidak ada keranjang</td>
-                            </tr>
+                                <tr>
+                                    <td colspan="6" style="text-align: center">Tidak ada keranjang</td>
+                                </tr>
                             @endif
-                        </tbody>
-                    </table>
-                    <div class="card-body border-top">
-                        <form action="{{ URL::to('checkout') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="id_keranjang[]" id="id_keranjang">
-                            <button class="btn btn-primary float-md-right" type="submit">Lanjut Checkout <i
+                            </tbody>
+                        </table>
+                        <div class="card-body border-top">
+                            {{--                        <form action="{{ URL::to('checkout') }}" method="post">--}}
+                            {{--                            @csrf--}}
+                            {{--                            <input type="hidden" name="id_keranjang[]" id="id_keranjang">--}}
+                            {{--                            <button class="btn btn-primary float-md-right" type="submit">Lanjut Checkout <i--}}
+                            {{--                                        class="fa fa-chevron-right"></i></button>--}}
+                            {{--                        </form>--}}
+                            <button class="btn btn-primary float-md-right" type="submit" id="bayar">Lanjut Checkout <i
                                         class="fa fa-chevron-right"></i></button>
-                        </form>
-                        <a href="{{ URL::to('produk') }}" class="btn btn-light"> <i class="fa fa-chevron-left"></i>
-                            Lanjut Belanja </a>
-                    </div>
-                </div> <!-- card.// -->
-                <div class="alert alert-success mt-3">
-                    <p class="icontext"><i class="icon text-success fa fa-truck"></i> Free Delivery within 1-2 weeks
-                    </p>
-                </div>
-            </main> <!-- col.// -->
-            <aside class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <dl class="dlist-align">
-                            <dt>Total:</dt>
-                            <dd class="text-right  h5" id="total">@currency($total)</dd>
-                        </dl>
-                        <hr>
-                        <p class="text-center mb-3">
-                            <img src="{{ asset('assets/mpnj/images/misc/payments.png') }}" height="26">
+                            <a href="{{ URL::to('produk') }}" class="btn btn-light"> <i class="fa fa-chevron-left"></i>
+                                Lanjut Belanja </a>
+                        </div>
+                    </div> <!-- card.// -->
+                    <div class="alert alert-success mt-3">
+                        <p class="icontext"><i class="icon text-success fa fa-truck"></i> Free Delivery within 1-2 weeks
                         </p>
-                    </div> <!-- card-body.// -->
-                </div> <!-- card .// -->
-            </aside> <!-- col.// -->
-        </div>
-    </div> <!-- container .//  -->
-</section>
+                    </div>
+                </main> <!-- col.// -->
+                <aside class="col-md-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <dl class="dlist-align">
+                                <dt>Total:</dt>
+                                <dd class="text-right  h5" id="total">@currency($total)</dd>
+                            </dl>
+                            <hr>
+                            <p class="text-center mb-3">
+                                <img src="{{ asset('assets/mpnj/images/misc/payments.png') }}" height="26">
+                            </p>
+                        </div> <!-- card-body.// -->
+                    </div> <!-- card .// -->
+                </aside> <!-- col.// -->
+            </div>
+        </div> <!-- container .//  -->
+    </section>
 @endsection
 
+<div class="modal fade" id="modalIdKosong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Gagal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Anda harus memilih minimal 1 barang pada keranjang untuk melakukan transaksi.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @push('scripts')
-<script>
-    var totalPrice = 0;
+    <script>
+        var totalPrice = 0;
         $(function () {
             var jml = 0;
 
@@ -137,15 +161,15 @@
                 $("#checkout").prop('disabled', true);
             }
 
-            var id_keranjang = [];
+            var keranjang_id = [];
+
             $("input:checkbox[name=check]:checked").each(function () {
-                id_keranjang.push($(this).val());
+                keranjang_id.push($(this).val());
             });
 
-            $('#id_keranjang').val(JSON.stringify(id_keranjang));
-
             $("input:checkbox[name=check]").on('click', function () {
-                let keranjang_id =  [];
+                // let keranjang_id = [];
+                keranjang_id = [];
                 let id = $(this).val();
                 let total = 0;
                 // $(`#check${id}`).attr('checked', false);
@@ -156,7 +180,7 @@
                 keranjang_id.forEach(function (val) {
                     total += $(`#data_keranjang${val}`).data('subtotal');
                 });
-                $('#id_keranjang').val(JSON.stringify(keranjang_id));
+
                 $("#total").html("Rp. " + numberFormat(parseInt(total)));
 
             });
@@ -187,6 +211,31 @@
                         updateJumlah(id_cart, qty, diskon, n);
                     }
                 }
+            });
+
+            $("#bayar").on('click', function () {
+                $.ajax({
+                    async: true,
+                    type: 'POST',
+                    url: '{{ URL::to('keranjang/go_checkout') }}',
+                    data: {
+                        'id_keranjang' : keranjang_id
+                    },
+                    beforeSend: function() {
+                        $.LoadingOverlay("show");
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function (response, xhr) {
+                        $.LoadingOverlay("hide");
+                        window.location.href = '{{ URL::to('checkout') }}'
+                    },
+                    error: function (error) {
+                        $.LoadingOverlay("hide");
+                        $("#modalIdKosong").modal('show');
+                    }
+                })
             });
 
         });
@@ -233,5 +282,5 @@
             console.log(totalPrice);
             $("#total").html("Rp. " + numberFormat(totalPrice));
         }
-</script>
+    </script>
 @endpush
