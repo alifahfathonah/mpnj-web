1f91c274e93bbd593dea2f9a5f8f8aefb3d9160d
LuthfiRobit <luthfirobit@gmail.com>
2020-07-09 01:17:42 +0700

    buat respon reviewNull

--- app/Repositories/ReviewRepository.php
+++ app/Repositories/ReviewRepository.php
@@ -36,6 +36,24 @@ class ReviewRepository
                 }
             );
     }
+
+    public function reviewNull($id_produk)
+    {
+        return Review::where('produk_id', $id_produk)
+            ->with('produk', 'produk.foto_produk', 'produk.kategori', 'user')
+            ->get()
+            ->map(
+                function ($review) {
+                    return [
+                        'produk' => [
+                            'id_produk' => $review->produk->id_produk,
+                            'nama_produk' => $review->produk->nama_produk,
+                            'kategori' => $review->produk->kategori->nama_kategori,
+                            'foto' => $review->produk->foto_produk[0]->foto_produk,
+                            'id_pelapak' => $review->user->id_user,
+                            'nama_toko' => $review->user->nama_toko,
+                        ],
+                        'review' => null
                     ];
                 }
             );
