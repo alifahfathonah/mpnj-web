a90c6ca10145af6b1ac6e30f1897d93b9b6bb4ba
LuthfiRobit <luthfirobit@gmail.com>
2020-07-11 01:57:20 +0700

    ganti response tampil wishlist seperti produk detail

--- app/Repositories/WishlistRepository.php
+++ app/Repositories/WishlistRepository.php
@@ -20,10 +20,33 @@ class WishlistRepository
                         'id_user' => $wishlist->user_id,
                         'id_produk' => $wishlist->produk_id,
                         'nama_produk' => $wishlist->produk->nama_produk,
+                        'kategori' => [
+                            'id_kategori' => $wishlist->produk->kategori->id_kategori_produk,
+                            'nama_kategori' => $wishlist->produk->kategori->nama_kategori
+                        ],
+                        'satuan' => $wishlist->produk->satuan,
+                        'berat' => $wishlist->produk->berat,
+                        'harga_modal' => $wishlist->produk->harga_modal,
                         'harga_jual' => $wishlist->produk->harga_jual,
+                        'stok' => $wishlist->produk->stok,
                         'diskon' => $wishlist->produk->diskon,
-                        'kategori' => $wishlist->produk->kategori->nama_kategori,
-                        'foto_produk' => $wishlist->produk->foto_produk[0]->foto_produk
+                        'keterangan' => $wishlist->produk->keterangan,
+                        'tipe_produk' => $wishlist->produk->tipe_produk,
+                        'wishlist' => $wishlist->produk->wishlist,
+                        'terjual' => $wishlist->produk->terjual,
+                        'foto' => $wishlist->produk->foto_produk->map(function ($foto) {
+                            return [
+                                'id_foto_poroduk' => $foto->id_foto_produk,
+                                'foto_produk' => $foto->foto_produk
+                            ];
+                        }),
+                        'pelapak' => [
+                            'id_pelapak' => $wishlist->produk->user->id_user,
+                            'nama_toko' => $wishlist->produk->user->nama_toko,
+                            'foto_pelapak' => asset('assets/foto_profil_konsumen/' . $wishlist->produk->user->foto_profil),
+                            'alamat' => $wishlist->produk->user->alamatToko->alamat_lengkap,
+                            'bergabung' => $wishlist->produk->user->created_at
+                        ]
                     ];
                 }
             );
