24a8faa2f568ea2f9603ce28027761bf0f7522bd
LuthfiRobit <luthfirobit@gmail.com>
2020-07-14 21:15:43 +0700

    view komplain

---
+++ resources/views/web/profile/komplain.blade.php
@@ -0,0 +1,28 @@
+<div class="card">
+    <div class="card-header p-2">
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link" href="#activity" data-toggle="tab">Activity</a></li>
+            <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+        </ul>
+    </div><!-- /.card-header -->
+    <div class="card-body">
+        <div class="tab-content">
+            <div class="tab-pane" id="activity">
+                <h1>1</h1>
+            </div>
+            <!-- /.tab-pane -->
+            <div class="tab-pane" id="timeline">
+                <!-- The timeline -->
+                <h1>2</h1>
+            </div>
+            <!-- /.tab-pane -->
+            <div class="tab-pane active" id="settings">
+                <h1>3</h1>
+            </div>
+            <!-- /.tab-pane -->
+        </div>
+        <!-- /.tab-content -->
+    </div><!-- /.card-body -->
+</div>
+<!-- /.nav-tabs-custom -->
