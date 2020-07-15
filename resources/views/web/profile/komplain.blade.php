5c3bf9594bc40e671c4cc20e71d33169e8446532
LuthfiRobit <luthfirobit@gmail.com>
2020-07-16 00:26:02 +0700

    komplain blade

--- resources/views/web/profile/komplain.blade.php
+++ resources/views/web/profile/komplain.blade.php
@@ -1,15 +1,15 @@
 <div class="card">
     <div class="card-header p-2">
         <ul class="nav nav-pills">
-            <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Semua</a></li>
-            <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Butuh Direspon</a></li>
-            <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Sudah Dibaca</a></li>
-            <li class="nav-item"><a class="nav-link" href="#setting" data-toggle="tab">Selesai</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#all" data-toggle="tab">Semua</a></li>
+            <li class="nav-item"><a class="nav-link" href="#respon" data-toggle="tab">Butuh Direspon</a></li>
+            <li class="nav-item"><a class="nav-link" href="#baca" data-toggle="tab">Sudah Dibaca</a></li>
+            <li class="nav-item"><a class="nav-link" href="#selesai" data-toggle="tab">Selesai</a></li>
         </ul>
     </div><!-- /.card-header -->
     <div class="card-body">
         <div class="tab-content">
-            <div class="tab-pane active" id="activity">
+            <div class="tab-pane active" id="all">
                 <div class="table-responsive">
                     <table class="table table-borderless">
                         <thead class="text">
@@ -22,28 +22,39 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach($komplain as $k)
                             <tr>
-                                <td> <a>1594163289</a> </td>
+                                <td> <a>{{$k->transaksi->kode_transaksi}}</a> </td>
                                 <td>
                                     <figure class="itemside">
-                                        <div class="aside"><img src="" class="img-xs border">
+                                        <div class="aside"><img
+                                                src="{{ asset('assets/foto_toko/'.$k->user->foto_toko) }}"
+                                                class="img-xs border">
                                         </div>
                                         <figcaption class="info">
-                                            <a href="" class="title text-dark">Toko</a>
+                                            <a href="" class="title text-dark">{{$k->user->nama_toko}}</a>
                                         </figcaption>
                                     </figure>
                                 </td>
                                 <td>
                                     <figure class="itemside">
-                                        <div class="aside"><img src="" class="img-xs border">
+                                        <div class="aside"><img
+                                                src="{{ env('FILES_ASSETS').$k->produk->foto_produk[0]->foto_produk }}"
+                                                class="img-xs border">
                                         </div>
                                         <figcaption class="info">
-                                            <a href="" class="title text-dark">Nama Barang</a>
+                                            <a href="" class="title text-dark">{{$k->produk->nama_produk}}</a>
                                         </figcaption>
                                     </figure>
                                 </td>
                                 <td>
+                                    @if ($k->komplain == 'Tidak Sesuai')
                                     <var class="price">Tidak Sesuai Deskripsi</var>
+                                    @elseIf($k->komplain == 'Tidak Lengkap')
+                                    <var class="price">Produk Tidak Lengkap</var>
+                                    @elseIf($k->komplain == 'Rusak')
+                                    <var class="price">Produk Rusak</var>
+                                    @endif
                                 </td>
                                 <td class="text-right">
                                     <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light"
@@ -51,21 +62,204 @@
                                     <a href="" class="btn btn-light"> Remove</a>
                                 </td>
                             </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
             <!-- /.tab-pane -->
-            <div class="tab-pane" id="timeline">
-                <!-- The timeline -->
-                <h1>2</h1>
+            <div class="tab-pane" id="respon">
+                @if($komplain_respon != null)
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead class="text">
+                            <tr class="small">
+                                <th scope="col" width="120">Kode Transaksi</th>
+                                <th scope="col">Toko</th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Komplain</th>
+                                <th scope="col"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($komplain_respon as $k)
+                            <tr>
+                                <td> <a>{{$k->transaksi->kode_transaksi}}</a> </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ asset('assets/foto_toko/'.$k->user->foto_toko) }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->user->nama_toko}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ env('FILES_ASSETS').$k->produk->foto_produk[0]->foto_produk }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->produk->nama_produk}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    @if ($k->komplain == 'Tidak Sesuai')
+                                    <var class="price">Tidak Sesuai Deskripsi</var>
+                                    @elseIf($k->komplain == 'Tidak Lengkap')
+                                    <var class="price">Produk Tidak Lengkap</var>
+                                    @elseIf($k->komplain == 'Rusak')
+                                    <var class="price">Produk Rusak</var>
+                                    @endif
+                                </td>
+                                <td class="text-right">
+                                    <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light"
+                                        data-toggle="tooltip"> <i class="fa fa-heart"></i></a>
+                                    <a href="" class="btn btn-light"> Remove</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                @else
+                <div class="info-main">
+                    <h4 class="h5 title mr-5"> Belum Ada Komplain Direspon</h4>
+                </div>
+                @endif
             </div>
             <!-- /.tab-pane -->
-            <div class="tab-pane" id="settings">
-                <h1>3</h1>
+            <div class="tab-pane" id="baca">
+                @if($komplain_dibaca != null)
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead class="text">
+                            <tr class="small">
+                                <th scope="col" width="120">Kode Transaksi</th>
+                                <th scope="col">Toko</th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Komplain</th>
+                                <th scope="col"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($komplain_dibaca as $k)
+                            <tr>
+                                <td> <a>{{$k->transaksi->kode_transaksi}}</a> </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ asset('assets/foto_toko/'.$k->user->foto_toko) }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->user->nama_toko}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ env('FILES_ASSETS').$k->produk->foto_produk[0]->foto_produk }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->produk->nama_produk}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    @if ($k->komplain == 'Tidak Sesuai')
+                                    <var class="price">Tidak Sesuai Deskripsi</var>
+                                    @elseIf($k->komplain == 'Tidak Lengkap')
+                                    <var class="price">Produk Tidak Lengkap</var>
+                                    @elseIf($k->komplain == 'Rusak')
+                                    <var class="price">Produk Rusak</var>
+                                    @endif
+                                </td>
+                                <td class="text-right">
+                                    <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light"
+                                        data-toggle="tooltip"> <i class="fa fa-heart"></i></a>
+                                    <a href="" class="btn btn-light"> Remove</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                @else
+                <div class="info-main">
+                    <h4 class="h5 title mr-5"> Belum Ada Komplain Dibaca</h4>
+                </div>
+                @endif
             </div>
-            <div class="tab-pane" id="setting">
-                <h1>3</h1>
+            <div class="tab-pane" id="selesai">
+                @if($komplain_selesai != null)
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead class="text">
+                            <tr class="small">
+                                <th scope="col" width="120">Kode Transaksi</th>
+                                <th scope="col">Toko</th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Komplain</th>
+                                <th scope="col"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($komplain_selesai as $k)
+                            <tr>
+                                <td> <a>{{$k->transaksi->kode_transaksi}}</a> </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ asset('assets/foto_toko/'.$k->user->foto_toko) }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->user->nama_toko}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img
+                                                src="{{ env('FILES_ASSETS').$k->produk->foto_produk[0]->foto_produk }}"
+                                                class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">{{$k->produk->nama_produk}}</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    @if ($k->komplain == 'Tidak Sesuai')
+                                    <var class="price">Tidak Sesuai Deskripsi</var>
+                                    @elseIf($k->komplain == 'Tidak Lengkap')
+                                    <var class="price">Produk Tidak Lengkap</var>
+                                    @elseIf($k->komplain == 'Rusak')
+                                    <var class="price">Produk Rusak</var>
+                                    @endif
+                                </td>
+                                <td class="text-right">
+                                    <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light"
+                                        data-toggle="tooltip"> <i class="fa fa-heart"></i></a>
+                                    <a href="" class="btn btn-light"> Remove</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                @else
+                <div class="info-main">
+                    <h4 class="h5 title mr-5"> Belum Ada Komplain Selesai</h4>
+                </div>
+                @endif
             </div>
             <!-- /.tab-pane -->
         </div>
