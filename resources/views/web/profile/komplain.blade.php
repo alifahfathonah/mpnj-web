f3e1573951782e508b6c3a3a10f6d74c6448a0a7
LuthfiRobit <luthfirobit@gmail.com>
2020-07-15 15:45:20 +0700

    komplain blade semua

--- resources/views/web/profile/komplain.blade.php
+++ resources/views/web/profile/komplain.blade.php
@@ -1,15 +1,59 @@
 <div class="card">
     <div class="card-header p-2">
         <ul class="nav nav-pills">
-            <li class="nav-item"><a class="nav-link" href="#activity" data-toggle="tab">Activity</a></li>
-            <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
-            <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Semua</a></li>
+            <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Butuh Direspon</a></li>
+            <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Sudah Dibaca</a></li>
+            <li class="nav-item"><a class="nav-link" href="#setting" data-toggle="tab">Selesai</a></li>
         </ul>
     </div><!-- /.card-header -->
     <div class="card-body">
         <div class="tab-content">
-            <div class="tab-pane" id="activity">
-                <h1>1</h1>
+            <div class="tab-pane active" id="activity">
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead class="text">
+                            <tr class="small">
+                                <th scope="col" width="120">Kode Transaksi</th>
+                                <th scope="col">Toko</th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Komplain</th>
+                                <th scope="col"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> <a>1594163289</a> </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img src="" class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">Toko</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <figure class="itemside">
+                                        <div class="aside"><img src="" class="img-xs border">
+                                        </div>
+                                        <figcaption class="info">
+                                            <a href="" class="title text-dark">Nama Barang</a>
+                                        </figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <var class="price">Tidak Sesuai Deskripsi</var>
+                                </td>
+                                <td class="text-right">
+                                    <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light"
+                                        data-toggle="tooltip"> <i class="fa fa-heart"></i></a>
+                                    <a href="" class="btn btn-light"> Remove</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <!-- /.tab-pane -->
             <div class="tab-pane" id="timeline">
@@ -17,7 +61,10 @@
                 <h1>2</h1>
             </div>
             <!-- /.tab-pane -->
-            <div class="tab-pane active" id="settings">
+            <div class="tab-pane" id="settings">
+                <h1>3</h1>
+            </div>
+            <div class="tab-pane" id="setting">
                 <h1>3</h1>
             </div>
             <!-- /.tab-pane -->
