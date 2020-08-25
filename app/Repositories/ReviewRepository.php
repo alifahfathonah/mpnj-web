7dd01efd179c5a616ff41d20a5f678cc7949fbde
LuthfiRobit <luthfirobit@gmail.com>
2020-08-25 21:45:09 +0700

    ganti response review

--- app/Repositories/ReviewRepository.php
+++ app/Repositories/ReviewRepository.php
@@ -15,23 +15,13 @@ class ReviewRepository
             ->map(
                 function ($review) {
                     return [
-                        'produk' => [
-                            'id_produk' => $review->produk->id_produk,
-                            'nama_produk' => $review->produk->nama_produk,
-                            'kategori' => $review->produk->kategori->nama_kategori,
-                            'foto' => $review->produk->foto_produk[0]->foto_produk,
-                            'id_pelapak' => $review->user->id_user,
-                            'nama_toko' => $review->user->nama_toko,
-                        ],
-                        'review' => [
-                            'id_review' => $review->id_review,
-                            'id_produk' => $review->produk_id,
-                            'id_user' => $review->user_id,
-                            'review' => $review->review,
-                            'bintang' => $review->bintang,
-                            'foto_review' => $review->foto_review
-                        ]
-
+                        'id_review' => $review->id_review,
+                        'id_produk' => $review->produk_id,
+                        'id_user' => $review->user_id,
+                        'review' => $review->review,
+                        'bintang' => $review->bintang,
+                        'foto_review' => $review->foto_review,
+                        'reviewer' => $review->user->nama_lengkap
                     ];
                 }
             );
@@ -44,17 +34,7 @@ class ReviewRepository
             ->get()
             ->map(
                 function ($review) {
-                    return [
-                        'produk' => [
-                            'id_produk' => $review->produk->id_produk,
-                            'nama_produk' => $review->produk->nama_produk,
-                            'kategori' => $review->produk->kategori->nama_kategori,
-                            'foto' => $review->produk->foto_produk[0]->foto_produk,
-                            'id_pelapak' => $review->user->id_user,
-                            'nama_toko' => $review->user->nama_toko,
-                        ],
-                        'review' => null
-                    ];
+                    return null;
                 }
             );
     }
